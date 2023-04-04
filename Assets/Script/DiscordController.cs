73898d42eb8f7e5dce5bc60881572841f54d9f16
EliteAsian <lavasnakegaming@gmail.com>
2023-04-03 21:18:12 -0400

    Fixed Discord crash

--- Assets/Script/DiscordController.cs
+++ Assets/Script/DiscordController.cs
@@ -3,55 +3,72 @@ using Discord;
 using UnityEngine;
 
 public class DiscordController : MonoBehaviour {
-	private Discord.Discord discord;
-	private ActivityManager activityManager;
-	private Activity activity;
+	public static DiscordController Instance {
+		get;
+		private set;
+	}
 
-	private float updateTime = 0f;
-	private long elaspedPlayTime = 0;
+	private Discord.Discord discord;
+
+	public Activity CurrentActivity {
+		set {
+			if (discord == null) {
+				return;
+			}
+
+			discord.GetActivityManager().UpdateActivity(value, result => {
+				if (result != Result.Ok) {
+					Debug.LogWarning("Discord Activity Error: " + result);
+				}
+			});
+		}
+	}
 
 	[SerializeField]
 	private long applicationID = 1091177744416637028;
+
+	// At the moment there is no changing of menus, what song is playing, etc. Basic for now.
 	[Space]
 	[SerializeField]
-	// At the moment there is no changing of menus, what song is playing, etc. Basic for now.
 	private string details = "Hello there ladies and gentlemen!";
+
 	[SerializeField]
 	private string state = "Are you ready to rock?";
+
+	// only shown in 'view profile' -> 'activity'
 	[Space]
 	[SerializeField]
-	// only shown in 'view profile' -> 'activity'
 	private string largeImage = "logo";
-	[SerializeField]
+
 	// tooltip for largeimage
+	[SerializeField]
 	private string largeText = "Yet Another Rhythm Game";
-	[SerializeField]
+
 	// Current instrument icon? only shown in 'view profile' -> 'activity' as a little overlay on the bottom right of the Large image - currently unused.
-	private string smallImage = "";
 	[SerializeField]
+	private string smallImage = "";
+
 	// Tooltip for smallImage - currently unused.
+	[SerializeField]
 	private string smallText = "";
 
 	private void Start() {
-		InitDiscord();
-	}
+		Instance = this;
 
-	private void InitDiscord() {
+		// Create the Discord instance
 		try {
-			// When the game is started while discord is already running the status update happens nearly instantly.
-			// However if discord is opened or re-opened after game start, it can take up to 35 seconds for the status to display.
-			discord?.Dispose();
-
-			// If discord isn't open at run start this will throw 'InternalError' instead of 'NotRunning' (don't know why)
 			discord = new Discord.Discord(applicationID, (ulong) CreateFlags.NoRequireDiscord);
-		} catch {
+		} catch (Exception e) {
+			Debug.LogWarning("Failed to start Discord presence client.");
+			Debug.LogException(e);
+
 			discord = null;
 			return;
 		}
 
-		activityManager = discord.GetActivityManager();
-		elaspedPlayTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-		activity = new Activity {
+		// Start the activity
+		long elaspedPlayTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+		CurrentActivity = new Activity {
 			Details = details,
 			State = state,
 			Assets = {
@@ -64,45 +81,36 @@ public class DiscordController : MonoBehaviour {
 		};
 	}
 
-	public void Update() {
-		TimedUpdateActivity();
+	private void Update() {
+		if (discord == null) {
+			return;
+		}
 
 		try {
-			if (discord == null) {
-				InitDiscord();
-				return;
-			}
-
 			discord.RunCallbacks();
 		} catch (Exception e) {
-			discord = null;
-			if (e.Message == "NotRunning") {
-				InitDiscord();
-			}
+			Debug.LogException(e);
+
+			TryDispose();
 		}
 	}
 
-	private void TimedUpdateActivity() {
-		// Discord has a rate limit of 5 updates per 20 seconds. 
-		// Need to make sure it isn't more otherwise Discord will freak the hell out and the activity will never update.
-
-		updateTime -= Time.deltaTime;
-		if (updateTime <= 0f) {
-			// null catch if discord wasn't open when game started. 
-			// Also, the results could be put into a log file or Debug.Log but it doesn't seem useful at the moment.
-
-			activityManager?.UpdateActivity(activity, (result) => { });
-			updateTime = 4.5f;
+	private void TryDispose() {
+		if (discord == null) {
+			return;
 		}
-	}
 
-	private void OnApplicationQuit() {
 		try {
-			activityManager?.ClearActivity((result) => { });
-			discord?.Dispose();
+			discord.GetActivityManager().ClearActivity((result) => { });
+			discord.Dispose();
+			discord = null;
 		} catch (Exception e) {
 			Debug.Log("Failed to clear activity or dispose of Discord.");
 			Debug.LogException(e);
 		}
+	}
+
+	private void OnApplicationQuit() {
+		TryDispose();
 	}
 }
