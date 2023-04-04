57534123cbc0aa6b5af9735d883b1387f07ce934
First Last <sillybegani@gmail.com>
2023-04-03 22:03:13 -0700

    barebones start lmao

---
+++ Assets/Script/Serialization/SongsDta.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using IniParser;
+using IniParser.Model;
+using UnityEngine;
+using YARG.Data;
+
+namespace YARG.Serialization {
+    public static class SongsDta {
+    }
+}
