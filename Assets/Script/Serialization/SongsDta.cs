cc72e043e4d2328bbb79d2b5f6f222e5fa214536
First Last <sillybegani@gmail.com>
2023-04-04 10:42:18 -0700

    please, for the love of god, squash these commits when merging this PR

--- Assets/Script/Serialization/SongsDta.cs
+++ Assets/Script/Serialization/SongsDta.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using IniParser;
 using IniParser.Model;
 using UnityEngine;
@@ -9,5 +10,6 @@ using YARG.Data;
 
 namespace YARG.Serialization {
     public static class SongsDta {
+		static Regex dtaMatchRegex = new Regex(@"\(.*\b\n");
     }
 }
