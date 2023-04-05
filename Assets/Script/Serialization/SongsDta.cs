7381fc351ad2dad022586e55c851553880247623
ieee802dot11ac <69217234+ieee802dot11ac@users.noreply.github.com>
2023-04-04 19:26:23 -0700

    some semblance of file parsing!
    still have to deal with multisong packs but hush

--- Assets/Script/Serialization/SongsDta.cs
+++ Assets/Script/Serialization/SongsDta.cs
@@ -19,9 +19,31 @@ namespace YARG.Serialization {
                     dtaFile = temp.ReadToEnd();
                 }
                 string[] tokens = dtaFile.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+                SongInfo newSong = new SongInfo(srcfolder);
+                string songPath = "";
+                int nestDepth = 0;
+                int localSongId = 0;
+                bool inSongSubdef = false;
                 for (int i = 0; i < tokens.Length; i++) {
                     Debug.Log("token index " + i + " , token " + tokens[i]);
+                    string token = tokens[i];
+                    if (token.Contains("(song")) {
+                        inSongSubdef = true;
+                    }
+                    if (token.Contains("(name") && inSongSubdef == false) {
+                        newSong.SongName = token.Split('"')[1];
+                        Debug.Log("song name: " + newSong.SongName);
+                    }
+                    if (token.Contains("(name") && inSongSubdef == true) {
+                        songPath = token.Split("(name")[1].TrimStart().Replace(")","");
+                        Debug.Log("song data location, ignoring extensions: " + songPath);
+                    }
+                    if (token.Contains("(artist")) {
+                        newSong.artistName = token.Split('"')[1];
+                        Debug.Log("song artist: " + newSong.artistName);
+                    }
                 }
+                songList.Add(newSong);
                 return songList;
             } catch (Exception e) {
 				Debug.LogError($"Failed to parse songs.dta for `{srcfolder.FullName}`.");
