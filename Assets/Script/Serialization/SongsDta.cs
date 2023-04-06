5acfb9668d6e7c83d9a7a7eb3c54202be8f907f5
First Last <sillybegani@gmail.com>
2023-04-06 10:33:53 -0700

    hope this doesn't choke and die on latin-1 characters lol

--- Assets/Script/Serialization/SongsDta.cs
+++ Assets/Script/Serialization/SongsDta.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 using YARG.Data;
 using Mackiloha;
 using DtxCS;
+using DtxCS.DataTypes;
 
 namespace YARG.Serialization {
-    public static class SongsDta {
-		static SongsDta() {
-
-        }
-        public static List<SongInfo> ParseSongsDta(DirectoryInfo srcfolder) {
-            try {
-                List<SongInfo> songList = new List<SongInfo>();
-                Encoding dtaEnc = Encoding.GetEncoding("iso-8859-1");
-                using (FileStream str = File.OpenRead(Path.Combine(srcfolder.FullName, "songs.dta"), dtaEnc)) {
-                    DTX asdf = DTX.FromDtaStream(str);
-                }
-                string dtaFile = asdf.ToString();
-                return songList;
-            } catch (Exception e) {
+	public static class SongsDta {
+		static SongsDta() {}
+		public static List<SongInfo> ParseSongsDta(DirectoryInfo srcfolder) {
+			try {
+				List<SongInfo> songList = new List<SongInfo>();
+				// Encoding dtaEnc = Encoding.GetEncoding("iso-8859-1");
+				DataArray asdf = new DataArray();
+				using (FileStream str = new FileStream(Path.Combine(srcfolder.FullName, "songs.dta"), FileMode.Open)) {
+					asdf = DTX.FromDtaStream(str);
+				}
+				string dtaFile = asdf.ToString();
+				return songList;
+			} catch (Exception e) {
 				Debug.LogError($"Failed to parse songs.dta for `{srcfolder.FullName}`.");
 				Debug.LogException(e);
-                return null;
+				return null;
 			}
-        }
-    }
+		}
+	}
 }
