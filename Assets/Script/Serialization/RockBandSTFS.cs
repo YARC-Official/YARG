1121bd0225f2370031048dba1f4a05b441f86347
ieee802dot11ac <69217234+ieee802dot11ac@users.noreply.github.com>
2023-04-08 14:51:54 -0700

    thanks emma lol

--- Assets/Script/Serialization/RockBandSTFS.cs
+++ Assets/Script/Serialization/RockBandSTFS.cs
@@ -4,9 +4,9 @@ using System.IO;
 using System.Text;
 using UnityEngine;
 using YARG.Data;
-using Mackiloha;
-using Mackiloha.IO;
-using Mackiloha.App;
+// using Mackiloha;
+// using Mackiloha.IO;
+// using Mackiloha.App;
 using DtxCS;
 using DtxCS.DataTypes;
 
@@ -17,21 +17,24 @@ namespace YARG.Serialization {
 			try {
 				List<SongInfo> songList = new List<SongInfo>();
 				// Encoding dtaEnc = Encoding.GetEncoding("iso-8859-1");
-				DataArray asdf = new DataArray();
+				DataArray dtaTree = new DataArray();
 				using (FileStream str = new FileStream(Path.Combine(srcfolder.FullName, "songs.dta"), FileMode.Open)) {
-					asdf = DTX.FromDtaStream(str);
-				}
-				string dtaFile = asdf.ToString();
+					dtaTree = DTX.FromDtaStream(str);
+				}					
+				for (int i = 0; i < dtaTree.Count; i++) {
+					DataNode dtaChildren = dtaTree[i];
+					Debug.Log($"dtaChildren = {dtaChildren.Name}");
+				};
 				string songPath = "songs/test/test";
 				string songPathGen = "songs/" + songPath.Split("/")[1] + "/gen/" + songPath.Split("/")[2];
-				Mackiloha.IO.SystemInfo sysInfo = new Mackiloha.IO.SystemInfo {
-					Version = 25, 
-					Platform = Platform.X360, 
-					BigEndian = true
-				};
-				var bitmap = new MiloSerializer(sysInfo).ReadFromFile<HMXBitmap>(Path.Combine(Path.Combine(srcfolder.FullName, songPathGen), "_keep.png_xbox"));
-				string tmpFilePath = Path.GetTempFileName() + ".png";
-           		bitmap.SaveAs(sysInfo, tmpFilePath);
+				// Mackiloha.IO.SystemInfo sysInfo = new Mackiloha.IO.SystemInfo {
+				// 	Version = 25, 
+				// 	Platform = Platform.X360, 
+				// 	BigEndian = true
+				// };
+				// var bitmap = new MiloSerializer(sysInfo).ReadFromFile<HMXBitmap>(Path.Combine(Path.Combine(srcfolder.FullName, songPathGen), "_keep.png_xbox"));
+				// string tmpFilePath = Path.GetTempFileName() + ".png";
+           		// bitmap.SaveAs(sysInfo, tmpFilePath);
 				return songList;
 			} catch (Exception e) {
 				Debug.LogError($"Failed to parse songs.dta for `{srcfolder.FullName}`.");
