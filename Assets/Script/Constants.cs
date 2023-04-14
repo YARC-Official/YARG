f3cf78b9647ced0709ce2a3ffef0f591fe92f2f0
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-04-14 18:13:23 +0100

    Created update checker, runs on startup

---
+++ Assets/Script/Constants.cs
@@ -0,0 +1,11 @@
+namespace YARG {
+	public static class Constants {
+
+		public const int MAJOR_VERSION = 0;
+		public const int MINOR_VERSION = 8;
+		public const int PATCH_VERSION = 1;
+
+		public static readonly string VERSION_TAG = $"v{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}";
+
+	}	
+}
