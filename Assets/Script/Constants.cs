3bcfe3d698de5c17c2b9ee34f814433e491dbafb
Raphael Goulart <contact@raphaelgoul.art>
2023-04-21 18:54:18 -0300

    FIve-fret Anti-ghosting (#160)
    * WIP anti-ghosting (pending hopo chords)
    
    * WIP anti-ghosting (pending hopo chords)
    
    * make anti-ghosting take taps into account
    
    * Take chord hopos/taps into account
    
    * ^
    
    * Make anti-ghosting togglable

--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -9,5 +9,6 @@ namespace YARG {
 		public const bool ANCHORING = true;
 		public const bool INFINITE_FRONTEND = false;
 		public const bool ANCHOR_CHORD_HOPO = true;
+		public const int EXTRA_ALLOWED_GHOSTS = 0;
 	}
 }
