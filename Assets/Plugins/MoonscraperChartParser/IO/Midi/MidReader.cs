1dc2b628d8dff78584abed11bfe9e0b57631ca84
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-04-26 17:09:48 +0100

    Add back message processing

--- Assets/Plugins/MoonscraperChartParser/IO/Midi/MidReader.cs
+++ Assets/Plugins/MoonscraperChartParser/IO/Midi/MidReader.cs
@@ -274,10 +274,48 @@ namespace MoonscraperChartEditor.Song.IO
                         break;
                 }
             }
+            
+            // Display messages to user
+            ProcessPendingUserMessages(messageList, ref callBackState);
 
             return moonSong;
         }
 
+        static void ProcessPendingUserMessages(IList<MessageProcessParams> messageList, ref CallbackState callBackState)
+        {
+	        if (messageList == null)
+	        {
+		        Debug.Assert(false, $"No message list provided to {nameof(ProcessPendingUserMessages)}!");
+		        return;
+	        }
+
+	        foreach (var processParams in messageList)
+	        {
+#if UNITY_EDITOR
+		        // The editor freezes when its message box API is used during parsing,
+		        // we use the params to determine whether or not to execute actions instead
+		        if (!processParams.executeInEditor)
+		        {
+			        Debug.Log("Auto-skipping action for message: " + processParams.message);
+			        continue;
+		        }
+		        else
+		        {
+			        Debug.Log("Auto-executing action for message: " + processParams.message);
+			        processParams.processFn(processParams);
+		        }
+#else
+                // callBackState = CallbackState.WaitingForExternalInformation;
+                // NativeMessageBox.Result result = NativeMessageBox.Show(processParams.message, processParams.title, NativeMessageBox.Type.YesNo, null);
+                // callBackState = CallbackState.None;
+                // if (result == NativeMessageBox.Result.Yes)
+                // {
+                //     processParams.processFn(processParams);
+                // }
+#endif
+	        }
+        }
+        
         static void ReadTrack(IList<MidiEvent> track)
         {
             foreach (var me in track)
