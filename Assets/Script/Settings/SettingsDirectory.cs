82a20fc1e10203fae85a04fbc955c7e01577ed18
EliteAsian <lavasnakegaming@gmail.com>
2023-05-03 21:39:13 -0400

    Remove duplicate files and unused imports

--- Assets/Script/Settings/SettingsDirectory.cs
+++ Assets/Script/Settings/SettingsDirectory.cs
@@ -1,6 +1,4 @@
-using System.IO;
 using System.Linq;
-using Cysharp.Threading.Tasks;
 using SFB;
 using TMPro;
 using UnityEngine;
--- Assets/Script/Song/Scanning/SongScanner.cs
+++ Assets/Script/Song/Scanning/SongScanner.cs
@@ -2,31 +2,28 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Threading;
-using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace YARG.Song {
 	public class SongScanner {
-		
+
 		private const int MAX_THREAD_COUNT = 2;
-		
+
 		private readonly ICollection<string> _songFolders;
 
 		private SongScanThread[] _scanThreads;
-		
+
 		public int TotalFoldersScanned { get; private set; }
 		public int TotalSongsScanned { get; private set; }
 		public int TotalErrorsEncountered { get; private set; }
 
 		private bool _isScanning;
 		private bool _hasScanned;
-		
+
 		public SongScanner(ICollection<string> songFolders) {
 			_songFolders = songFolders;
-			
+
 			Debug.Log("Initialized SongScanner");
 		}
 
@@ -39,7 +36,7 @@ namespace YARG.Song {
 			if (_scanThreads == null) {
 				return;
 			}
-			
+
 			foreach (var thread in _scanThreads) {
 				thread?.Abort();
 			}
@@ -55,17 +52,17 @@ namespace YARG.Song {
 				Debug.LogError("No song folders added to SongScanner");
 				return songs;
 			}
-			
+
 			_scanThreads = new SongScanThread[MAX_THREAD_COUNT];
-			
-			for(int i = 0; i < _scanThreads.Length; i++) {
+
+			for (int i = 0; i < _scanThreads.Length; i++) {
 				_scanThreads[i] = new SongScanThread(fast);
 			}
 
 			AssignFoldersToThreads();
 
 			_isScanning = true;
-			
+
 			foreach (var scanThread in _scanThreads) {
 				scanThread.StartScan();
 			}
@@ -81,8 +78,8 @@ namespace YARG.Song {
 				TotalFoldersScanned = 0;
 				TotalSongsScanned = 0;
 				TotalErrorsEncountered = 0;
-				
-				foreach(var thread in _scanThreads) {
+
+				foreach (var thread in _scanThreads) {
 					TotalFoldersScanned += thread.foldersScanned;
 					TotalSongsScanned += thread.songsScanned;
 					TotalErrorsEncountered += thread.errorsEncountered;
@@ -92,17 +89,17 @@ namespace YARG.Song {
 
 				await UniTask.NextFrame();
 			}
-			
+
 			// All threads have finished here
 
-			foreach(var thread in _scanThreads) {
+			foreach (var thread in _scanThreads) {
 				songs.AddRange(thread.Songs);
 			}
 
 			_isScanning = false;
 			_hasScanned = true;
 			Debug.Log("FINISHED SCANNING");
-			
+
 			// TODO output song errors at some point
 
 			_scanThreads = null;
@@ -116,7 +113,7 @@ namespace YARG.Song {
 
 		private void AssignFoldersToThreads() {
 			var drives = DriveInfo.GetDrives();
-			
+
 			var driveFolders = drives.ToDictionary(drive => drive, _ => new List<string>());
 
 			foreach (var folder in _songFolders) {
@@ -125,24 +122,24 @@ namespace YARG.Song {
 					Debug.LogError($"Folder {folder} is not on a drive");
 					continue;
 				}
-				
+
 				driveFolders[drive].Add(folder);
 			}
 
 			int threadIndex = 0;
 			using var enumerator = driveFolders.GetEnumerator();
-			
+
 			while (enumerator.MoveNext() && enumerator.Current.Key is not null) {
 				// No folders for this drive
 				if (enumerator.Current.Value.Count == 0) {
 					continue;
 				}
-				
+
 				// Add every folder from this drive to the thread
 				enumerator.Current.Value.ForEach(x => _scanThreads[threadIndex].AddFolder(x));
-				
+
 				threadIndex++;
-				
+
 				if (threadIndex >= MAX_THREAD_COUNT) {
 					threadIndex = 0;
 				}
@@ -158,11 +155,11 @@ namespace YARG.Song {
 		NoNotesFile,
 		NoAudioFile,
 	}
-	
+
 	public struct SongError {
 		public string Directory { get; }
 		public ScanResult Result { get; }
-		
+
 		public SongError(string directory, ScanResult result) {
 			Directory = directory;
 			Result = result;
