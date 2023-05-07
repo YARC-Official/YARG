6605477964cac0c2632e0b1b46ae4d360a149ffb
EliteAsian <lavasnakegaming@gmail.com>
2023-05-06 22:38:15 -0400

    Did someone say UPDATE?

--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -2,7 +2,7 @@ using YARG.Data;
 
 namespace YARG {
 	public static class Constants {
-		public static readonly YargVersion VERSION_TAG = YargVersion.Parse("v0.10.0b");
+		public static readonly YargVersion VERSION_TAG = YargVersion.Parse("v0.10.0");
 
 		// General
 		public const float HIT_MARGIN = 0.095f;
