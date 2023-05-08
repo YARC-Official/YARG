70ba2e320e6d23194a6c9440d79fcdb7cc82b7c4
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-05-08 20:40:08 +0100

    Fix caches not being scanned in parallel on error (#279)

--- Assets/Script/Song/SongContainer.cs
+++ Assets/Script/Song/SongContainer.cs
@@ -46,7 +46,7 @@ namespace YARG.Song {
 			return output.ErroredCaches;
 		}
 
-		public static async UniTask ScanFolders(ICollection<string> folders, Action<SongScanner> updateUi = null) {
+		public static async UniTask ScanFolders(ICollection<string> folders, bool fast, Action<SongScanner> updateUi = null) {
 			var songsToRemove = _songs.Where(song => folders.Contains(song.CacheRoot)).ToList();
 			
 			_songs.RemoveAll(x => songsToRemove.Contains(x));
@@ -55,12 +55,12 @@ namespace YARG.Song {
 			}
 			
 			var scanner = new SongScanner(folders);
-			var songs = await scanner.StartScan(false, updateUi);
+			var songs = await scanner.StartScan(fast, updateUi);
 
 			AddSongs(songs.SongEntries);
 		}
 		
-		public static async UniTask ScanSingleFolder(string path, Action<SongScanner> updateUi = null) {
+		public static async UniTask ScanSingleFolder(string path, bool fast, Action<SongScanner> updateUi = null) {
 			var songsToRemove = _songs.Where(song => song.CacheRoot == path).ToList();
 
 			_songs.RemoveAll(x => songsToRemove.Contains(x));
@@ -69,7 +69,7 @@ namespace YARG.Song {
 			}
 
 			var scanner = new SongScanner(new[] { path });
-			var songs = await scanner.StartScan(false, updateUi);
+			var songs = await scanner.StartScan(fast, updateUi);
 
 			AddSongs(songs.SongEntries);
 		}
