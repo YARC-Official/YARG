7c43aeab160af2159134c2181f59ec8a10cccb4b
rjkiv <karrenbauerr@gmail.com>
2023-05-08 00:17:29 -0400

    CON scanning now returns a List of ConSongEntry objects

--- Assets/Script/Serialization/Xbox/XboxCONFileBrowser.cs
+++ Assets/Script/Serialization/Xbox/XboxCONFileBrowser.cs
@@ -33,31 +33,41 @@ namespace YARG.Serialization {
 					// Get song metadata from songs.dta
 					ConSongEntry currentSong = XboxDTAParser.ParseFromDta(currentArray);
 					
-					// TODO: alongside this stuff below, make sure you assign file size and mem block offsets!
-					// // since Location is currently set to the name of the folder before mid/mogg/png, set those paths now
-					// currentSong.NotesFile = Path.Combine(folder, currentSong.Location, $"{currentSong.Location}.mid");
-					// currentSong.MoggPath = Path.Combine(folder, currentSong.Location, $"{currentSong.Location}.mogg");
-					// string imgPath = Path.Combine(folder, currentSong.Location, "gen", $"{currentSong.Location}_keep.png_xbox");
-					// if(currentSong.HasAlbumArt && File.Exists(imgPath))
-					// 	currentSong.ImagePath = imgPath;
+					// since Location is currently set to the name of the folder before mid/mogg/png, set those paths now
+					// since we're dealing with a CON and not an ExCON, grab each relevant file's sizes and memory block offsets
+					currentSong.NotesFile = Path.Combine("songs", currentSong.Location, $"{currentSong.Location}.mid");
+					currentSong.MidiFileSize = theCON.GetFileSize(currentSong.NotesFile);
+					currentSong.MidiFileMemBlockOffsets = theCON.GetMemOffsets(currentSong.NotesFile);
 
-					// // Get song folder path for mid, mogg, png_xbox
-					// currentSong.Location = Path.Combine(folder, currentSong.Location);
+					currentSong.MoggPath = Path.Combine("songs", currentSong.Location, $"{currentSong.Location}.mogg");
+					currentSong.MoggFileSize = theCON.GetFileSize(currentSong.MoggPath);
+					currentSong.MoggFileMemBlockOffsets = theCON.GetMemOffsets(currentSong.MoggPath);
+
+					string imgPath = Path.Combine("songs", currentSong.Location, "gen", $"{currentSong.Location}_keep.png_xbox");
+					currentSong.ImageFileSize = theCON.GetFileSize(imgPath);
+					currentSong.ImageFileMemBlockOffsets = theCON.GetMemOffsets(imgPath);
+
+					if(currentSong.HasAlbumArt && currentSong.ImageFileSize > 0 && currentSong.ImageFileMemBlockOffsets != null)
+						currentSong.ImagePath = imgPath;
+
+					// Set this song's "Location" to the path of the CON file
+					currentSong.Location = conName;
 					
-					// // Parse the mogg
-					// using var fs = new FileStream(currentSong.MoggPath, FileMode.Open, FileAccess.Read);
-					// using var br = new BinaryReader(fs);
+					// Parse the mogg
+					using var fs = new FileStream(conName, FileMode.Open, FileAccess.Read);
+					using var br = new BinaryReader(fs);
+					fs.Seek(currentSong.MoggFileMemBlockOffsets[0], SeekOrigin.Begin);
 
-					// currentSong.MoggHeader = br.ReadInt32();
-					// currentSong.MoggAddressAudioOffset = br.ReadInt32();
-					// currentSong.MoggAudioLength = fs.Length - currentSong.MoggAddressAudioOffset;
-					// MoggBASSInfoGenerator(currentSong, currentArray.Array("song"));
+					currentSong.MoggHeader = br.ReadInt32();
+					currentSong.MoggAddressAudioOffset = br.ReadInt32();
+					currentSong.MoggAudioLength = currentSong.MoggFileSize - currentSong.MoggAddressAudioOffset;
+					MoggBASSInfoGenerator.Generate(currentSong, currentArray.Array("song"));
 
-					// // Debug.Log($"{currentSong.ShortName}:\nMidi path: {currentSong.NotesFile}\nMogg path: {currentSong.MoggPath}\nImage path: {currentSong.ImagePath}");
+					// Debug.Log($"{currentSong.ShortName}:\nMidi path: {currentSong.NotesFile}\nMogg path: {currentSong.MoggPath}\nImage path: {currentSong.ImagePath}");
 
-					// // will validate the song outside of this class, in SongScanThread.cs
-					// // so okay to add to song list for now
-					// songList.Add(currentSong);
+					// will validate the song outside of this class, in SongScanThread.cs
+					// so okay to add to song list for now
+					songList.Add(currentSong);
 				} catch (Exception e) {
 					Debug.Log($"Failed to load song, skipping...");
 					Debug.LogException(e);
@@ -68,46 +78,3 @@ namespace YARG.Serialization {
 		}
 	}
 }
-
-// namespace YARG.Serialization {
-//     public static class XboxCONFileBrowser {
-// 		public static List<XboxCONSong> BrowseCON(string conName) {
-//             var songList = new List<XboxCONSong>();
-// 			var dtaTree = new DataArray();
-
-//             // Attempt to read songs.dta
-//             STFS thisCON = new STFS(conName);
-//             try{
-//                 dtaTree = DTX.FromPlainTextBytes(thisCON.GetFile("songs/songs.dta"));
-//                 Debug.Log("Successfully read dta");
-// 			} catch (Exception e) {
-// 				Debug.LogError($"Failed to parse songs.dta for `{conName}`.");
-// 				Debug.LogException(e);
-// 				return null;
-// 			}
-            
-//             // Read each song the dta file lists
-//             for(int i = 0; i < dtaTree.Count; i++){
-//                 try {
-// 					var currentArray = (DataArray) dtaTree[i];
-//                     var currentSong = new XboxCONSong(conName, currentArray, thisCON);
-//                     currentSong.ParseSong();
-
-// 					if (currentSong.IsValidSong()) {
-// 						songList.Add(currentSong);
-// 					} else {
-// 						Debug.LogError($"Song with shortname `{currentSong.shortname}` is invalid. Skipping.");
-// 					}
-// 				} catch (Exception e) {
-// 					Debug.Log($"Failed to load song, skipping...");
-// 					Debug.LogException(e);
-// 				}
-//             }
-
-//             // XboxCONSong lol = new XboxCONSong(conName, (DataArray)dtaTree[dtaTree.Count - 1], thisCON);
-//             // lol.GetMoggFile();
-
-// 			return songList;
-//         }
-//     }
-// }
