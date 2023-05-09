69679db501e354b063edbc2f93fda2e6f546dac4
NevesPT <davi.106@hotmail.com>
2023-05-09 21:00:06 +0100

    Dev Watermark
    [Added]
    - a script to check if is beta version or not
    
    [Changed]
    - Elements at persistant Scene

---
+++ Assets/Script/UI/DevWatermark.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using YARG;
+using Debug = UnityEngine.Debug;
+
+public class DevWatermark : MonoBehaviour
+{
+
+    public TextMeshProUGUI watermarkText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // check if Constants.VERSION_TAG ends with "b"
+        if (Constants.VERSION_TAG.ToString().EndsWith("b"))
+        {
+            watermarkText.text = $"<b>YARG {Constants.VERSION_TAG}</b>  Developer Build";
+            watermarkText.gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+        
+        // disable script
+        this.enabled = false;
+        Debug.Log("DevWatermark script disabled");
+    }
+}   
