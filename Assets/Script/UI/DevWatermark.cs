2f2f7d9b9305126feac7c157e9495e5796b381b1
NevesPT <davi.106@hotmail.com>
2023-05-11 22:40:21 +0100

    Fix following styling

--- Assets/Script/UI/DevWatermark.cs
+++ Assets/Script/UI/DevWatermark.cs
@@ -5,27 +5,23 @@ using UnityEngine;
 using YARG;
 using Debug = UnityEngine.Debug;
 
-public class DevWatermark : MonoBehaviour
-{
+public class DevWatermark : MonoBehaviour {
 
-    public TextMeshProUGUI watermarkText;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        // check if Constants.VERSION_TAG ends with "b"
-        if (Constants.VERSION_TAG.beta)
-        {
-            watermarkText.text = $"<b>YARG {Constants.VERSION_TAG}</b>  Developer Build";
-            watermarkText.gameObject.SetActive(true);
-        }
-        else
-        {
-            this.gameObject.SetActive(false);
-        }
-        
-        // disable script
-        this.enabled = false;
-        Debug.Log("DevWatermark script disabled");
-    }
-}   
+	[SerializeField]
+	private TextMeshProUGUI watermarkText;
+
+	// Start is called before the first frame update
+	void Start() {
+		// check if Constants.VERSION_TAG ends with "b"
+		if (Constants.VERSION_TAG.beta) {
+			watermarkText.text = $"<b>YARG {Constants.VERSION_TAG}</b>  Developer Build";
+			watermarkText.gameObject.SetActive(true);
+		} else {
+			this.gameObject.SetActive(false);
+		}
+
+		// disable script
+		this.enabled = false;
+		Debug.Log("DevWatermark script disabled");
+	}
+}
