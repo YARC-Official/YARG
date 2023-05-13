345c843ed49a8594eb2e38e5376a1d547610e155
Jalen Jackson <jalenjacksoncs@gmail.com>
2023-05-13 18:02:01 -0400

    Set preview duration to 30 seconds

--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -17,5 +17,8 @@ namespace YARG {
 		public const bool ALLOW_DESC_GHOSTS = true;
 		public const bool ALLOW_GHOST_IF_NO_NOTES = true; // seems to allow more ghosting than it should...
 		public const float ALLOW_GHOST_IF_NO_NOTES_THRESHOLD = 2f; // this should solve the above
+		
+		// Preview
+		public const double PREVIEW_DURATION = 30.0;
 	}
 }
