555cbcbda1f7f1805b3e45bab450d8f482c28919
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-05-23 10:32:25 -0500

    add additional light calls to not be stuck on strobe

--- Assets/Script/Serialization/Parser/MidiParser.Venue.cs
+++ Assets/Script/Serialization/Parser/MidiParser.Venue.cs
@@ -42,6 +42,63 @@ namespace YARG.Serialization.Parser {
 					case "strobe_fast":
 						eventName = "venue_light_strobeFast";
 						break;
+					case "verse":
+						eventName = "venue_light_verse";
+						break;
+					case "chorus":
+						eventName = "venue_light_chorus";
+						break;
+					case "manual_cool":
+						eventName = "venue_light_manual_cool";
+						break;
+					case "manual_warm":
+						eventName = "venue_light_manual_warm";
+						break;
+					case "dischord":
+						eventName = "venue_light_dischord";
+						break;
+					case "loop_cool":
+						eventName = "venue_light_loop_cool";
+						break;
+					case "silhouettes":
+						eventName = "venue_light_silhouettes";
+						break;
+					case "loop_warm":
+						eventName = "venue_light_loop_warm";
+						break;
+					case "silhouettes_spot":
+						eventName = "venue_light_silhouettes_spot";
+						break;
+					case "frenzy":
+						eventName = "venue_light_frenzy";
+						break;
+					case "blackout_fast":
+						eventName = "venue_light_blackout_fast";
+						break;
+					case "flare_fast":
+						eventName = "venue_light_flare_fast";
+						break;
+					case "searchlights":
+						eventName = "venue_light_searchlights";
+						break;
+					case "flare_slow":
+						eventName = "venue_light_flare_slow";
+						break;
+					case "harmony":
+						eventName = "venue_light_harmony";
+						break;
+					case "sweep":
+						eventName = "venue_light_sweep";
+						break;
+					case "bre":
+						eventName = "venue_light_bre";
+						break;
+					case "strobe_slow":
+						eventName = "venue_light_strobe_slow";
+						break;
+					case "blackout_slow":
+						eventName = "venue_light_blackout_slow";
+						break;
 					case "stomp":
 						eventName = "venue_light_onOffMode";
 						break;
