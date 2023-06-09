f74bd04465e6713bcb5167820e5b313d904ecd13
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-06-08 22:47:37 -0400

    Neon light support

---
+++ Assets/Script/Venue/Lights/NeonLightManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace YARG.Venue {
+	public class NeonLightManager : MonoBehaviour {
+		private static readonly int EmissionMultiplier = Shader.PropertyToID("_Emission_Multiplier");
+		private static readonly int EmissionSecondaryColor = Shader.PropertyToID("_Emission_Secondary_Color");
+
+		[SerializeField]
+		private Material[] _neonMaterials;
+
+		private void Update() {
+			var lightState = LightManager.Instance.MainLightState;
+
+			// Update all of the materials
+			foreach (var material in _neonMaterials) {
+				material.SetFloat(EmissionMultiplier, lightState.Intensity);
+
+				if (lightState.Color == null) {
+					material.SetColor(EmissionSecondaryColor, Color.white);
+				} else {
+					material.SetColor(EmissionSecondaryColor, lightState.Color.Value);
+				}
+			}
+		}
+	}
+}
