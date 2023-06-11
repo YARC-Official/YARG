73c1db9eaf192df1bebd131c63feb4cad1d38c55
Nathan <nathanhurst03@gmail.com>
2023-06-11 13:41:59 -0600

    Implement .chart lyrics parsing

--- Assets/Script/Data/GenericLyricInfo.cs
+++ Assets/Script/Data/GenericLyricInfo.cs
@@ -4,6 +4,10 @@ namespace YARG.Data {
 	public class GenericLyricInfo : AbstractInfo {
 		public List<(float time, string word)> lyric;
 
+		public GenericLyricInfo() {
+			lyric = new();
+		}
+
 		public GenericLyricInfo(float time, float length, List<(float, string)> lyric) {
 			this.time = time;
 			this.length = length;
