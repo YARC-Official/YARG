8e3ab752b65ebfbdb60d1db822a6d5c7b8cb8b14
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-06-24 21:39:58 -0500

    unfurl the track on enter

--- Assets/Script/PlayMode/CameraPositioner.cs
+++ Assets/Script/PlayMode/CameraPositioner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
@@ -5,13 +6,21 @@ using YARG.Settings;
 
 namespace YARG.PlayMode {
 	public class CameraPositioner : MonoBehaviour {
-		private static List<CameraPositioner> cameraPositioners = new();
+		private static List<CameraPositioner> cameraPositioners = new List<CameraPositioner>();
+		private bool hasPlayedEntryAnimation = false; // Flag to track if the animation has played
 
 		private void Start() {
 			cameraPositioners.Add(this);
 
 			UpdateAntiAliasing();
-			UpdatePosition();
+
+			// Check if the entry animation has already played
+			if (!hasPlayedEntryAnimation) {
+				StartCoroutine(PlayEntryAnimation());
+				hasPlayedEntryAnimation = true; // Set the flag to true after playing the animation
+			} else {
+				UpdatePosition(); // If the animation has already played, set the position directly
+			}
 		}
 
 		private void OnDestroy() {
@@ -40,6 +49,58 @@ namespace YARG.PlayMode {
 
 			// Rotation
 			transform.localRotation = Quaternion.Euler(SettingsManager.Settings.TrackCamRot.Data, 0f, 0f);
+		}
+
+		private IEnumerator PlayEntryAnimation() {
+			// Hide the camera
+			GetComponent<Camera>().enabled = false;
+
+			// Calculate the target position
+			float targetY = SettingsManager.Settings.TrackCamYPos.Data;
+			float targetZ = SettingsManager.Settings.TrackCamZPos.Data - 6f;
+			Vector3 targetPosition = new Vector3(0f, targetY, targetZ);
+
+			// Calculate the starting position (above the target)
+			Vector3 startingPosition = new Vector3(0f, targetY + 3f, targetZ + 5f); // Adjust the translation distance here
+
+			// Store the final position before hiding the camera
+			Vector3 finalPosition = targetPosition;
+
+			// Set the camera position to the starting position
+			transform.localPosition = startingPosition;
+
+
+			// Show the camera
+			GetComponent<Camera>().enabled = true;
+
+			// Update the position and store the correct final position
+			UpdatePosition();
+			UpdateAllPosition();
+			finalPosition = transform.localPosition;
+
+			// Snap the camera to an initial position
+			transform.localPosition = finalPosition + new Vector3(0f, 3f, 5f);
+
+			// Entry animation duration (2 seconds)
+			float animationDuration = 2.5f;
+			float animationStartTime = Time.time;
+
+			while (Time.time - animationStartTime < animationDuration) {
+				float t = (Time.time - animationStartTime) / animationDuration; // Interpolation factor (0 to 1)
+
+				// Apply a slower animation curve during move-in
+				float smoothT = Mathf.SmoothStep(0f, 1f, t);
+				smoothT = Mathf.Lerp(0f, smoothT, smoothT); // Apply additional smoothing
+
+
+				// Smoothly interpolate the camera position using the modified curve
+				transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition, smoothT);
+
+				yield return null;
+			}
+
+			// Ensure the final position is accurate
+			transform.localPosition = finalPosition;
 		}
 
 		public static void UpdateAllAntiAliasing() {
