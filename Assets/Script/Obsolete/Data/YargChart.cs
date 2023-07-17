d47646d571755533fb146303dc8423ea7bfd5aba
Nathan <nathanhurst03@gmail.com>
2023-07-16 18:06:32 -0600

    Remove obsolete .chart loaders

--- Assets/Script/Obsolete/Data/YargChart.cs
+++ Assets/Script/Obsolete/Data/YargChart.cs
@@ -1,21 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using MoonscraperChartEditor.Song;
-using YARG.Chart;
 using YARG.Gameplay;
 
 namespace YARG.Data
 {
     public sealed class YargChart
     {
-        // Matches text inside [brackets], not including the brackets
-        // '[end]' -> 'end', '[section Solo] - "Solo"' -> 'section Solo'
-        private static readonly Regex textEventRegex =
-            new(@"\[(.*?)\]", RegexOptions.Compiled | RegexOptions.Singleline);
-
-        private MoonSong _song;
-
         private List<List<NoteInfo>[]> allParts;
 
         public List<List<NoteInfo>[]> AllParts
@@ -37,93 +27,18 @@ namespace YARG.Data
             set => allParts = value;
         }
 
-        private List<NoteInfo>[] guitar;
+        public List<NoteInfo>[] Guitar = CreateArray();
+        public List<NoteInfo>[] GuitarCoop = CreateArray();
+        public List<NoteInfo>[] Rhythm = CreateArray();
+        public List<NoteInfo>[] Bass = CreateArray();
+        public List<NoteInfo>[] Keys = CreateArray();
 
-        public List<NoteInfo>[] Guitar
-        {
-            get => guitar ??= LoadArray(ChartLoader.GuitarLoader);
-            set => guitar = value;
-        }
+        public List<NoteInfo>[] RealGuitar = CreateArray();
+        public List<NoteInfo>[] RealBass = CreateArray();
 
-        private List<NoteInfo>[] guitarCoop;
-
-        public List<NoteInfo>[] GuitarCoop
-        {
-            get => guitarCoop ??= LoadArray(ChartLoader.GuitarCoopLoader);
-            set => guitarCoop = value;
-        }
-
-        private List<NoteInfo>[] rhythm;
-
-        public List<NoteInfo>[] Rhythm
-        {
-            get => rhythm ??= LoadArray(ChartLoader.RhythmLoader);
-            set => rhythm = value;
-        }
-
-        private List<NoteInfo>[] bass;
-
-        public List<NoteInfo>[] Bass
-        {
-            get => bass ??= LoadArray(ChartLoader.BassLoader);
-            set => bass = value;
-        }
-
-        private List<NoteInfo>[] keys;
-
-        public List<NoteInfo>[] Keys
-        {
-            get => keys ??= LoadArray(ChartLoader.KeysLoader);
-            set => keys = value;
-        }
-
-        private List<NoteInfo>[] realGuitar;
-
-        public List<NoteInfo>[] RealGuitar
-        {
-            get =>
-                realGuitar ??=
-                    CreateArray(); // TODO: Needs chartloaders once Pro Guitar parsing is implemented in the MS code
-            set => realGuitar = value;
-        }
-
-        private List<NoteInfo>[] realBass;
-
-        public List<NoteInfo>[] RealBass
-        {
-            get =>
-                realBass ??=
-                    CreateArray(); // TODO: Needs chartloaders once Pro Guitar parsing is implemented in the MS code
-            set => realBass = value;
-        }
-
-        private List<NoteInfo>[] drums;
-
-        public List<NoteInfo>[] Drums
-        {
-            get => drums ??= LoadArray(ChartLoader.DrumsLoader);
-            set => drums = value;
-        }
-
-        private List<NoteInfo>[] realDrums;
-
-        public List<NoteInfo>[] RealDrums
-        {
-            get => realDrums ??= LoadArray(ChartLoader.ProDrumsLoader);
-            set => realDrums = value;
-        }
-
-        private List<NoteInfo>[] ghDrums;
-
-        public List<NoteInfo>[] GhDrums
-        {
-            get => ghDrums ??= LoadArray(ChartLoader.FiveLaneDrumsLoader);
-            set => ghDrums = value;
-        }
-
-#pragma warning restore format
-
-        private List<string> _loadedEvents = new();
+        public List<NoteInfo>[] Drums = CreateArray(5);
+        public List<NoteInfo>[] RealDrums = CreateArray(5);
+        public List<NoteInfo>[] GhDrums = CreateArray(5);
 
         public List<EventInfo> events = new();
         public List<Beat> beats = new();
@@ -143,64 +58,6 @@ namespace YARG.Data
         /// </summary>
         public List<LyricInfo>[] harmLyrics = new List<LyricInfo>[0];
 
-        public YargChart(MoonSong song)
-        {
-            _song = song;
-            if (song == null)
-            {
-                return;
-            }
-
-            GenericLyricInfo currentLyricPhrase = null;
-            foreach (var globalEvent in song.eventsAndSections)
-            {
-                float time = (float) globalEvent.time;
-                string text = globalEvent.title;
-                // Strip away the [brackets] from events (and any garbage outside them)
-                var match = textEventRegex.Match(text);
-                if (match.Success)
-                {
-                    text = match.Groups[1].Value;
-                }
-
-                switch (text)
-                {
-                    case "phrase_start":
-                        // Phrase start events can start new phrases without a preceding end
-                        if (currentLyricPhrase != null)
-                        {
-                            currentLyricPhrase.length = time - currentLyricPhrase.time;
-                            genericLyrics.Add(currentLyricPhrase);
-                        }
-
-                        // Start new phrase
-                        currentLyricPhrase = new()
-                        {
-                            time = time
-                        };
-                        break;
-
-                    case "phrase_end":
-                        // Complete phrase and add it to the list
-                        currentLyricPhrase.length = time - currentLyricPhrase.time;
-                        genericLyrics.Add(currentLyricPhrase);
-                        currentLyricPhrase = null;
-                        break;
-
-                    // default:
-                    //     if (text.StartsWith(LyricHelper.LYRIC_EVENT_PREFIX))
-                    //     {
-                    //         // Add lyric to current phrase, or ignore if outside a phrase
-                    //         currentLyricPhrase?.lyric.Add((time, text.Replace(LyricHelper.LYRIC_EVENT_PREFIX, "")));
-                    //     }
-                    //
-                    //     break;
-                }
-
-                events.Add(new EventInfo(text, (float) globalEvent.time));
-            }
-        }
-
         public List<NoteInfo>[] GetChartByName(string name)
         {
             return name switch
@@ -217,49 +74,6 @@ namespace YARG.Data
                 "ghDrums"    => GhDrums,
                 _            => throw new InvalidOperationException($"Unsupported chart type `{name}`.")
             };
-        }
-
-        public void InitializeArrays()
-        {
-            Guitar = CreateArray();
-            GuitarCoop = CreateArray();
-            Rhythm = CreateArray();
-            Bass = CreateArray();
-            Keys = CreateArray();
-            RealGuitar = CreateArray();
-            RealBass = CreateArray();
-            Drums = CreateArray(5);
-            RealDrums = CreateArray(5);
-            GhDrums = CreateArray(5);
-        }
-
-        private List<NoteInfo>[] LoadArray(ChartLoader<NoteInfo> loader)
-        {
-            var maxDifficulty = loader.MaxDifficulty;
-            var instrument = loader.Instrument;
-            string instrumentName = loader.InstrumentName;
-
-            var notes = new List<NoteInfo>[(int) (maxDifficulty + 1)];
-            if (_song == null)
-            {
-                return notes;
-            }
-
-            for (Difficulty diff = Difficulty.EASY; diff <= maxDifficulty; diff++)
-            {
-                notes[(int) diff] = loader.GetNotesFromChart(_song, diff);
-            }
-
-            if (_loadedEvents.Contains(instrumentName))
-            {
-                return notes;
-            }
-
-            events.AddRange(loader.GetEventsFromChart(_song));
-            events.Sort((e1, e2) => e1.time.CompareTo(e2.time));
-            _loadedEvents.Add(instrumentName);
-
-            return notes;
         }
 
         private static List<NoteInfo>[] CreateArray(int length = 4)
