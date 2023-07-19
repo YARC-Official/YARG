ee1029348071ed2acb8d0de5edf3b749ed6aa73a
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-07-19 12:59:52 -0400

    Made fading per track + fixed beatline fading

--- Assets/Script/Gameplay/Visuals/TrackElements/TrackElement.cs
+++ Assets/Script/Gameplay/Visuals/TrackElements/TrackElement.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using YARG.Gameplay.Player;
+using YARG.Settings;
 
 namespace YARG.Gameplay.Visuals
 {
     public abstract class TrackElement<TPlayer> : MonoBehaviour, IPoolable
         where TPlayer : BasePlayer
     {
+        // TODO: We should probably move these somewhere else
+        private static readonly int _fadeZeroPosition = Shader.PropertyToID("_FadeZeroPosition");
+        private static readonly int _fadeFullPosition = Shader.PropertyToID("_FadeFullPosition");
+
         protected const float REMOVE_POINT = -4f;
 
         protected GameManager GameManager { get; private set;  }
@@ -23,6 +28,22 @@ namespace YARG.Gameplay.Visuals
 
             // Hide everything at the start
             HideElement();
+
+            // Get fade info
+            // TODO: Make this per player. This is why we don't use global shader values
+            float fadePos = SettingsManager.Settings.TrackFadePosition.Data;
+            float fadeSize = SettingsManager.Settings.TrackFadeSize.Data;
+
+            // Set all fade values
+            var meshRenderers = GetComponentsInChildren<MeshRenderer>();
+            foreach (var meshRenderer in meshRenderers)
+            {
+                foreach (var material in meshRenderer.materials)
+                {
+                    material.SetVector(_fadeZeroPosition, new Vector4(0f, 0f, fadePos, 0f));
+                    material.SetVector(_fadeFullPosition, new Vector4(0f, 0f, fadePos - fadeSize, 0f));
+                }
+            }
         }
 
         public void EnableFromPool()
