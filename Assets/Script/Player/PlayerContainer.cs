a6def91c9b5a37e032a16c854b8099a42e3e503e
Nathan <nathanhurst03@gmail.com>
2023-07-28 19:59:22 -0600

    Quick profile container cleanup

--- Assets/Script/Player/PlayerContainer.cs
+++ Assets/Script/Player/PlayerContainer.cs
@@ -14,10 +14,13 @@ namespace YARG.Player
     /// </summary>
     public static class PlayerContainer
     {
-        private static readonly List<YargProfile> _profiles;
-        private static readonly List<YargPlayer> _players;
+        private const string  PROFILES_FILE = "profiles.json";
+        private static string ProfilesPath => Path.Combine(PathHelper.PersistentDataPath, PROFILES_FILE);
 
-        private static readonly Dictionary<YargProfile, YargPlayer> _playersByProfile;
+        private static readonly List<YargProfile> _profiles = new();
+        private static readonly List<YargPlayer> _players = new();
+
+        private static readonly Dictionary<YargProfile, YargPlayer> _playersByProfile = new();
 
         /// <summary>
         /// A list of all of the profiles (taken or not).
@@ -29,19 +32,10 @@ namespace YARG.Player
         /// </summary>
         public static IReadOnlyList<YargPlayer> Players => _players;
 
-        static PlayerContainer()
-        {
-            _profiles = new List<YargProfile>();
-            _players = new List<YargPlayer>();
-            _playersByProfile = new Dictionary<YargProfile, YargPlayer>();
-        }
-
         public static bool AddProfile(YargProfile profile)
         {
             if (_profiles.Contains(profile))
-            {
                 return false;
-            }
 
             _profiles.Add(profile);
             return true;
@@ -50,15 +44,11 @@ namespace YARG.Player
         public static bool RemoveProfile(YargProfile profile)
         {
             if (!_profiles.Contains(profile))
-            {
                 return false;
-            }
 
             // A profile that is taken can't be removed
             if (_playersByProfile.ContainsKey(profile))
-            {
                 return false;
-            }
 
             _profiles.Remove(profile);
             return true;
@@ -72,14 +62,10 @@ namespace YARG.Player
         public static YargPlayer CreatePlayerFromProfile(YargProfile profile)
         {
             if (!_profiles.Contains(profile))
-            {
                 return null;
-            }
 
             if (IsProfileTaken(profile))
-            {
                 return null;
-            }
 
             var player = new YargPlayer(profile);
             _players.Add(player);
@@ -91,9 +77,7 @@ namespace YARG.Player
         public static bool DisposePlayer(YargPlayer player)
         {
             if (!_players.Contains(player))
-            {
                 return false;
-            }
 
             _players.Remove(player);
             _playersByProfile.Remove(player.Profile);
@@ -106,14 +90,10 @@ namespace YARG.Player
         public static bool SwapPlayerToProfile(YargPlayer player, YargProfile newProfile)
         {
             if (!_players.Contains(player))
-            {
                 return false;
-            }
 
             if (IsProfileTaken(newProfile))
-            {
                 return false;
-            }
 
             _playersByProfile.Remove(player.Profile);
             _playersByProfile.Add(newProfile, player);
@@ -126,9 +106,7 @@ namespace YARG.Player
         public static YargPlayer GetPlayerFromProfile(YargProfile profile)
         {
             if (!_playersByProfile.TryGetValue(profile, out var player))
-            {
                 return null;
-            }
 
             return player;
         }
@@ -141,30 +119,22 @@ namespace YARG.Player
             _players.ForEach(i => i.Dispose());
             _players.Clear();
 
-            string profilesPath = Path.Combine(PathHelper.PersistentDataPath, "profiles.json");
+            string profilesPath = ProfilesPath;
             if (!File.Exists(profilesPath))
-            {
                 return 0;
-            }
 
             string profilesJson = File.ReadAllText(profilesPath);
             var profiles = JsonConvert.DeserializeObject<List<YargProfile>>(profilesJson);
-
             if (profiles is not null)
-            {
                 _profiles.AddRange(profiles);
-            }
 
             return _profiles.Count;
         }
 
         public static int SaveProfiles()
         {
-            string profilesPath = Path.Combine(PathHelper.PersistentDataPath, "profiles.json");
-
             string profilesJson = JsonConvert.SerializeObject(_profiles, Formatting.Indented);
-
-            File.WriteAllText(profilesPath, profilesJson);
+            File.WriteAllText(ProfilesPath, profilesJson);
 
             return _profiles.Count;
         }
