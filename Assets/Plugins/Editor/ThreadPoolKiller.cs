54fefa32c2dae5f961fdc883f52757aa6f149ae6
Nathan <nathanhurst03@gmail.com>
2023-08-04 22:57:16 -0600

    Allow disabling force-reload, and add button to force-reload on-demand

--- Assets/Plugins/Editor/ThreadPoolKiller.cs
+++ Assets/Plugins/Editor/ThreadPoolKiller.cs
@@ -6,18 +6,50 @@ namespace YARG.Editor
     [InitializeOnLoad]
     public static class ThreadPoolKiller
     {
+        // TODO: The saved state doesn't seem to get re-loaded when restarting Unity
+        // Leaving this as-is for now, since the default is to not force-kill which is less obstructive
+        [FilePath("UserSettings/YARG/ThreadPoolKiller.asset", FilePathAttribute.Location.ProjectFolder)]
+        private class ThreadPoolKillerState : ScriptableSingleton<ThreadPoolKillerState>
+        {
+            [field: SerializeField]
+            public bool Enabled { get; private set; }
+
+            public bool Toggle()
+            {
+                Enabled = !Enabled;
+                Save(true);
+                return Enabled;
+            }
+        }
+
+        private const string FORCE_KILL = "YARG/Force-Reload Scripts";
+        private const string FORCE_KILL_ON_EXIT = "YARG/Force-Reload Scripts on Play Exit (to kill thread pool)";
+
         static ThreadPoolKiller()
         {
             EditorApplication.playModeStateChanged += PlayModeStateChanged;
+            Menu.SetChecked(FORCE_KILL_ON_EXIT, ThreadPoolKillerState.instance.Enabled);
+        }
+
+        [MenuItem(FORCE_KILL, false)]
+        private static void ForceKillThreadPool()
+        {
+            Debug.Log($"Forcing script reload to kill active thread pool threads...");
+            EditorUtility.RequestScriptReload();
+        }
+
+        [MenuItem(FORCE_KILL_ON_EXIT, false)]
+        private static void SetEnabled()
+        {
+            Menu.SetChecked(FORCE_KILL_ON_EXIT, ThreadPoolKillerState.instance.Toggle());
         }
 
         private static void PlayModeStateChanged(PlayModeStateChange newState)
         {
-            if (newState != PlayModeStateChange.ExitingPlayMode)
+            if (!ThreadPoolKillerState.instance.Enabled || newState != PlayModeStateChange.ExitingPlayMode)
                 return;
 
-            Debug.Log($"Forcing script reload to kill active thread pool threads...");
-            EditorUtility.RequestScriptReload();
+            ForceKillThreadPool();
         }
     }
 }
