0ef40962353f9a81106e83215b18f47f3fd5232b
sonicfind <sonicfind2@gmail.com>
2023-08-09 17:03:59 -0500

    [NO COMPILATION] Update Ouvert Export

--- Assets/Script/Song/OuvertExport.cs
+++ Assets/Script/Song/OuvertExport.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -28,7 +28,7 @@ namespace YARG.Song
 
             // public bool lyrics;
             [JsonProperty("songlength")]
-            public int songLength;
+            public ulong songLength;
         }
 
         public static void ExportOuvertSongsTo(string path)
@@ -45,8 +45,8 @@ namespace YARG.Song
                     album = song.Album,
                     genre = song.Genre,
                     charter = song.Charter,
-                    year = song.Year,
-                    songLength = (int) (song.SongLength * 1000f)
+                    year = song.UnmodifiedYear,
+                    songLength = song.SongLength
                 });
             }
 
