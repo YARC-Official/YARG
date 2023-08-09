9d4d6b82f999a7c145f77801969d821585045de8
sonicfind <sonicfind2@gmail.com>
2023-08-09 17:09:15 -0500

    [NO COMPILATION] Added & improved Sidebar album art generation code structure

--- Assets/Script/Obsolete/Serialization/Xbox/XboxImageGenerator.cs
+++ Assets/Script/Obsolete/Serialization/Xbox/XboxImageGenerator.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -9,39 +10,42 @@ using YARG.Audio;
 
 namespace YARG.Serialization
 {
+    public class XboxImageSettings
+    {
+        public readonly byte bitsPerPixel;
+        public readonly int format;
+        public readonly int width;
+        public readonly int height;
+        public unsafe XboxImageSettings(byte[] data)
+        {
+            unsafe
+            {
+                bitsPerPixel = data[1];
+                format = BinaryPrimitives.ReadInt32LittleEndian(new(data, 2, 4));
+                width = BinaryPrimitives.ReadInt16LittleEndian(new(data, 7, 2));
+                height = BinaryPrimitives.ReadInt16LittleEndian(new(data, 9, 2));
+            }
+        }
+    }
+
     public static class XboxImageTextureGenerator
     {
-        public static async UniTask<Texture2D> GetTexture(byte[] xboxImageBytes, CancellationToken ct)
+#nullable enable
+        public static unsafe XboxImageSettings? GetTexture(byte[] data, CancellationToken ct)
         {
-            var ms = new MemoryStream(xboxImageBytes);
-
-            // Parse header and get DXT blocks
-            byte[] header = ms.ReadBytes(32);
-            byte BitsPerPixel = header[1];
-            int Format = BitConverter.ToInt32(header, 2);
-            short Width = BitConverter.ToInt16(header, 7);
-            short Height = BitConverter.ToInt16(header, 9);
-            bool isDXT1 = ((BitsPerPixel == 0x04) && (Format == 0x08));
-            ms.Seek(32, SeekOrigin.Begin);
-            byte[] DXTBlocks = ms.ReadBytes((int) (ms.Length - 32));
-
-            ct.ThrowIfCancellationRequested();
-
             // Swap bytes because xbox is weird like that
-            for (int i = 0; i < DXTBlocks.Length / 2; i++)
+            byte buf;
+            for (int i = 32; i < data.Length; i += 2)
             {
-                (DXTBlocks[i * 2], DXTBlocks[i * 2 + 1]) = (DXTBlocks[i * 2 + 1], DXTBlocks[i * 2]);
+                if (ct.IsCancellationRequested)
+                    return null;
+
+                buf = data[i];
+                data[i] = data[i + 1];
+                data[i + 1] = buf;
             }
 
-            ct.ThrowIfCancellationRequested();
-
-            // apply DXT1 OR DXT5 formatted bytes to a Texture2D
-            var tex = new Texture2D(Width, Height,
-                (isDXT1) ? GraphicsFormat.RGBA_DXT1_SRGB : GraphicsFormat.RGBA_DXT5_SRGB, TextureCreationFlags.None);
-            tex.LoadRawTextureData(DXTBlocks);
-            tex.Apply();
-
-            return tex;
+            return new XboxImageSettings(data);
         }
     }
 }
