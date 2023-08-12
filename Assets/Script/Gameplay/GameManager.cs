84306ff2d0153fc495e97d5b10cc039f48b9c9bf
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-08-12 17:49:21 +0100

    Forgot one case of unpausing for practice

--- Assets/Script/Gameplay/GameManager.cs
+++ Assets/Script/Gameplay/GameManager.cs
@@ -234,6 +234,11 @@ namespace YARG.Gameplay
         {
             if (Keyboard.current.escapeKey.wasPressedThisFrame)
             {
+                if (IsPractice && !PracticeManager.HasSelectedSection)
+                {
+                    return;
+                }
+
                 SetPaused(!Paused);
             }
 
@@ -276,7 +281,8 @@ namespace YARG.Gameplay
             {
                 byte buttonMask = ((FiveFretPlayer) _players[0]).Engine.State.ButtonMask;
                 int noteIndex = ((FiveFretPlayer) _players[0]).Engine.State.NoteIndex;
-                _debugText.text = $"Note index: {noteIndex}\nButtons: {buttonMask}\n"
+                double starPower = ((FiveFretPlayer) _players[0]).Engine.EngineStats.StarPowerAmount;
+                _debugText.text = $"Note index: {noteIndex}\nButtons: {buttonMask}\nStar Power: {starPower:0.0000}\n"
                     + $"Input time: {InputTime:0.000000}\nSong time: {SongTime:0.000000}\nTime difference: {InputTime - SongTime:0.000000}\n"
                     + $"Speed adjustment: {_syncSpeedAdjustment:0.00}\nSpeed multiplier: {_syncSpeedMultiplier}\n"
                     + $"Sync start delta: {_syncStartDelta:0.000000}";
