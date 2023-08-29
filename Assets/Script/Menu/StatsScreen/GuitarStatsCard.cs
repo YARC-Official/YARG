6d1a9e4a353b01ae905a0398554d7a4c8ced1538
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-08-29 22:36:46 +0100

    Add StatsCard classes

---
+++ Assets/Script/Menu/StatsScreen/GuitarStatsCard.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using YARG.Core.Engine.Guitar;
+
+namespace YARG.Menu.StatsScreen
+{
+    public class GuitarStatsCard : StatsCard<GuitarStats>
+    {
+        [SerializeField]
+        private TextMeshProUGUI _overstrums;
+
+        [SerializeField]
+        private TextMeshProUGUI _hoposStrummed;
+
+        [SerializeField]
+        private TextMeshProUGUI _ghostInputs;
+
+        public override void SetCardContents()
+        {
+            base.SetCardContents();
+
+            _overstrums.text = Stats.Overstrums.ToString();
+            _hoposStrummed.text = Stats.HoposStrummed.ToString();
+            _ghostInputs.text = Stats.GhostInputs.ToString();
+        }
+    }
+}
---
+++ Assets/Script/Menu/StatsScreen/StatsCard.cs
@@ -0,0 +1,73 @@
+﻿using TMPro;
+using UnityEngine;
+using YARG.Core;
+using YARG.Core.Engine;
+using YARG.Data;
+using YARG.Player;
+
+namespace YARG.Menu.StatsScreen
+{
+
+    public abstract class StatsCard<T> : MonoBehaviour where T : BaseStats
+    {
+
+        [SerializeField]
+        private TextMeshProUGUI _playerName;
+
+        [SerializeField]
+        private TextMeshProUGUI _accuracyPercent;
+
+        [SerializeField]
+        private TextMeshProUGUI _score;
+
+        [SerializeField]
+        private TextMeshProUGUI _difficulty;
+
+        [SerializeField]
+        private TextMeshProUGUI _notesHit;
+
+        [SerializeField]
+        private TextMeshProUGUI _maxStreak;
+
+        [SerializeField]
+        private TextMeshProUGUI _notesMissed;
+
+        [SerializeField]
+        private TextMeshProUGUI _phrasesHit;
+
+        protected YargPlayer Player;
+        protected T Stats;
+
+        public void Initialize(YargPlayer player, T stats)
+        {
+            Player = player;
+            Stats = stats;
+        }
+
+        public virtual void SetCardContents()
+        {
+            _playerName.text = Player.Profile.Name;
+
+            // Set instrument icon
+
+            var totalNotes = Stats.NotesHit + Stats.NotesMissed;
+            if (totalNotes == 0)
+            {
+                _accuracyPercent.text = "0%";
+            }
+            else
+            {
+                _accuracyPercent.text = $"{Stats.NotesHit / totalNotes}%";
+            }
+
+            _difficulty.text = Player.Profile.Difficulty.ToDisplayName();
+
+            _notesHit.text = $"{Stats.NotesHit} / {totalNotes}";
+            _maxStreak.text = Stats.MaxCombo.ToString();
+            _notesMissed.text = Stats.NotesMissed.ToString();
+
+            _score.text = Stats.Score.ToString();
+        }
+
+    }
+}
