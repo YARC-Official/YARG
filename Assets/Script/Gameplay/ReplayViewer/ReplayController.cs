e67f63593e46c3a3985271cdb554a158956783c4
RileyTheFox <jacobchambers@hotmail.co.uk>
2023-09-01 03:01:01 +0100

    Most amazing looking replay viewer UI you have ever seen

---
+++ Assets/Script/Gameplay/ReplayViewer/ReplayController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using YARG.Core.Chart;
+using YARG.Core.Replays;
+
+namespace YARG.Gameplay.ReplayViewer
+{
+    public class ReplayController : MonoBehaviour
+    {
+        private GameManager _gameManager;
+        private Replay      _replay;
+
+        private void Awake()
+        {
+            _gameManager = FindObjectOfType<GameManager>();
+        }
+
+        private void Start()
+        {
+            if (!_gameManager.IsReplay)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            _gameManager.ChartLoaded += OnChartLoaded;
+        }
+
+        private void OnChartLoaded(SongChart chart)
+        {
+            _replay = _gameManager.Replay;
+
+            _gameManager.ChartLoaded -= OnChartLoaded;
+        }
+    }
+}
