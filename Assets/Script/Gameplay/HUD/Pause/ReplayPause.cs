6c2d19ccabf60e37eb9a12f5e3584382b673dd37
Paulo Cardoso <davi.106@hotmail.com>
2023-09-09 10:59:56 +0100

    [WIP] Added Replay Menu

---
+++ Assets/Script/Gameplay/HUD/Pause/ReplayPause.cs
@@ -0,0 +1,44 @@
+﻿using TMPro;
+using UnityEngine;
+using YARG.Settings.Customization;
+
+namespace YARG.Gameplay.HUD
+{
+    public class ReplayPause  : GenericPause
+    {
+        [SerializeField]
+        private GameObject saveColorObject;
+
+        protected override void GameplayAwake()
+        {
+            base.GameplayAwake();
+
+            if (!GameManager.IsReplay)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void SaveColorProfile()
+        {
+            // Get the 1st player
+            var thisPlayer = GameManager.Players[0];
+
+            // get the color profile
+            var colorProfile = thisPlayer.Player.ColorProfile;
+
+            // save the color profile
+            CustomContentManager.ColorProfiles.SaveItem(colorProfile);
+
+            // refresh the color profile
+            CustomContentManager.ColorProfiles.LoadFiles();
+
+            // get Name object then set text to Saved!
+            saveColorObject.GetComponentInChildren<TextMeshProUGUI>().text = "Saved!";
+
+            // remove the onclick listeners to prevent spamming
+            //saveColorObject.GetComponentInChildren<NavigatableButton>().RemoveOnClickListeners();
+
+        }
+    }
+}
