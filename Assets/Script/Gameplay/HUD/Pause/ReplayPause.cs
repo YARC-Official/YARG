ba095f8f7af6db2336365cd93bfc783ef811ca94
Paulo Cardoso <davi.106@hotmail.com>
2023-09-10 21:44:16 +0100

    Replaced the Line and Rework the Conditions

--- Assets/Script/Gameplay/HUD/Pause/ReplayPause.cs
+++ Assets/Script/Gameplay/HUD/Pause/ReplayPause.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using YARG.Core.Game;
+using YARG.Gameplay.Player;
 using YARG.Menu.Navigation;
 using YARG.Settings.Customization;
 
@@ -9,25 +10,38 @@ namespace YARG.Gameplay.HUD
     public class ReplayPause  : GenericPause
     {
         [SerializeField]
+        private GameObject _separatorObject;
+        [SerializeField]
         private GameObject _saveColorObject;
+        private BasePlayer _thisPlayer;
+        private ColorProfile _colorProfile;
+
+        // get _thisPlayer and _colorProfile before OnEnable
+        protected override void GameplayAwake()
+        {
+            base.GameplayAwake();
+
+            // get player info
+            _thisPlayer = GameManager.Players[0];
+            _colorProfile = _thisPlayer.Player.ColorProfile;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            // if ColorProfile is Default, remove the Save button
+            if (_colorProfile.Name == ColorProfile.Default.Name)
+            {
+                _separatorObject.SetActive(false);
+                _saveColorObject.SetActive(false);
+            }
+        }
 
         public void SaveColorProfile()
         {
-            // Get the 1st player
-            var thisPlayer = GameManager.Players[0];
-
-            // get the color profile
-            var colorProfile = thisPlayer.Player.ColorProfile;
-
-            // if ColorProfile is Default, destroy the saveColorObject and return
-            if (colorProfile.Name == ColorProfile.Default.Name)
-            {
-                Destroy(_saveColorObject);
-                return;
-            }
-
             // save the color profile
-            CustomContentManager.ColorProfiles.SaveItem(colorProfile);
+            CustomContentManager.ColorProfiles.SaveItem(_colorProfile);
 
             // refresh the color profile
             CustomContentManager.ColorProfiles.LoadFiles();
