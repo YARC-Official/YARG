5a4cc31fe48bc46919ffe96f4b04d91ee8170113
Nathan <nathanhurst03@gmail.com>
2023-09-14 03:04:45 -0600

    Automatically add YARG.Core projects to Unity-generated solution Avoids the need to switch between solutions

--- Assets/Plugins/Editor/YARGCoreBuilder.cs
+++ Assets/Plugins/Editor/YARGCoreBuilder.cs
@@ -16,7 +16,7 @@ using Debug = UnityEngine.Debug;
 namespace Editor
 {
     [InitializeOnLoad]
-    public class YARGCoreBuilder : IPreprocessBuildWithReport
+    public class YARGCoreBuilder : AssetPostprocessor, IPreprocessBuildWithReport
     {
         private const string OUTPUT_FOLDER = "Assets/Plugins/YARG.Core";
         private const string HASH_PATH = "Assets/Plugins/YARG.Core/YARG.Core.hash";
@@ -29,6 +29,8 @@ namespace Editor
             .GetType("NugetForUnity.UnityPreImportedLibraryResolver").GetMethod("GetAlreadyImportedLibs",
                 BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static, null, new Type[] { }, null)
             .CreateDelegate(typeof(UnityPreImportedLibraryResolver_GetAlreadyImportedLibs));
+
+        private static string ProjectRoot => Directory.GetParent(Application.dataPath)?.ToString();
 
         // For automatically building in the Editor upon any recompilations
         static YARGCoreBuilder()
@@ -53,6 +55,107 @@ namespace Editor
         [MenuItem("YARG/Rebuild YARG.Core (Release)", false)]
         public static void BuildRelease() => BuildYARGCoreDLL(force: true, debug: false);
 
+        // Undocumented post-process hooks called by IDE packages
+        // For the first two, return can be either void if no content modifications are made,
+        // or string if the contents are being modified
+
+        // Adds YARG.Core projects to the Unity solution
+        // Avoids having to switch between solutions constantly
+        private static string OnGeneratedSlnSolution(string path, string contents)
+        {
+            // Check for submodule
+            string projectRoot = ProjectRoot;
+            string submodule = Path.Combine(projectRoot, "YARG.Core");
+            if (!Directory.Exists(submodule))
+            {
+                Debug.LogError($"Submodule {"YARG.Core"} does not exist!");
+                return contents;
+            }
+
+            // Write to temporary file
+            string directory = Path.GetDirectoryName(path);
+            string tempFile = Path.Combine(directory, "temp.sln");
+            File.WriteAllText(tempFile, contents);
+
+            // Find submodule projects
+            // Collected separately so we can have a count
+            var projectFiles = new List<string>();
+            EditorUtility.DisplayProgressBar("Adding YARG.Core Projects to Solution", "Finding project files", 0f);
+            foreach (string folder in Directory.EnumerateDirectories(submodule, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                foreach (string projectFile in Directory.EnumerateFiles(folder, "*.csproj", SearchOption.TopDirectoryOnly))
+                {
+                    projectFiles.Add(projectFile);
+                }
+            }
+
+            // Add submodule projects
+            for (int i = 0; i < projectFiles.Count; i++)
+            {
+                string projectFile = projectFiles[i];
+                try
+                {
+                    EditorUtility.DisplayProgressBar("Adding YARG.Core Projects to Solution",
+                        $"Adding {Path.GetFileName(projectFile)} ({i + 1} of {projectFiles.Count})",
+                        i / projectFiles.Count);
+                    RunCommand("dotnet", @$"sln ""{tempFile}"" add ""{projectFile}""").Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Failed to add YARG.Core project {projectFile} to solution {path}");
+                    Debug.LogException(ex);
+                }
+            }
+            EditorUtility.ClearProgressBar();
+
+            // Read back temp file as new contents
+            contents = File.ReadAllText(tempFile);
+            File.Delete(tempFile);
+            return contents;
+        }
+
+        // Adds YARG.Core to each of the Unity project files
+        // Not really necessary and takes a long time, presumably since the project files are large
+        // private static string OnGeneratedCSProject(string path, string contents)
+        // {
+        //     // Check for submodule
+        //     string projectRoot = ProjectRoot;
+        //     string submodule = Path.Combine(projectRoot, "YARG.Core");
+        //     if (!Directory.Exists(submodule))
+        //     {
+        //         Debug.LogWarning($"Submodule {"YARG.Core"} does not exist!");
+        //         return contents;
+        //     }
+
+        //     // Write to temporary file
+        //     string directory = Path.GetDirectoryName(path);
+        //     string tempFile = Path.Combine(directory, "temp.csproj");
+        //     File.WriteAllText(tempFile, contents);
+
+        //     // Add YARG.Core reference
+        //     try
+        //     {
+        //         EditorUtility.DisplayProgressBar("Adding YARG.Core Reference to Project",
+        //             $"Adding YARG.Core to {Path.GetFileName(path)}", 0f);
+        //         string projectFile = Path.Join(submodule, "YARG.Core", $"YARG.Core.csproj");
+        //         RunCommand("dotnet", @$"add ""{tempFile}"" reference ""{projectFile}""").Dispose();
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Debug.LogError($"Failed to add YARG.Core project to project {path}");
+        //         Debug.LogException(ex);
+        //     }
+        //     finally
+        //     {
+        //         EditorUtility.ClearProgressBar();
+        //     }
+
+        //     // Read back temp file as new contents
+        //     contents = File.ReadAllText(tempFile);
+        //     File.Delete(tempFile);
+        //     return contents;
+        // }
+
         public static void BuildYARGCoreDLL(bool force = false, bool debug = true)
         {
             try
@@ -69,7 +172,7 @@ namespace Editor
                 Debug.Log("Rebuilding YARG.Core...");
 
                 // Get directories
-                string projectRoot = Directory.GetParent(Application.dataPath)?.ToString();
+                string projectRoot = ProjectRoot;
                 string submodulePath = Path.Join(projectRoot, "YARG.Core", "YARG.Core");
                 string projectPath = Path.Join(submodulePath, "YARG.Core.csproj");
 
