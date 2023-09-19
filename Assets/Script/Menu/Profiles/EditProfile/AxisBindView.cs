8dfe41e40f71aa50f112f949e881b4f84e8a7881
Nathan <nathanhurst03@gmail.com>
2023-09-19 00:06:24 -0600

    Expose axis calibration settings in binding menu

--- Assets/Script/Menu/Profiles/EditProfile/AxisBindView.cs
+++ Assets/Script/Menu/Profiles/EditProfile/AxisBindView.cs
@@ -1,12 +1,57 @@
+using UnityEngine;
 using YARG.Input;
 
 namespace YARG.Menu.Profiles
 {
     public class AxisBindView : BindView<float, AxisBinding, SingleAxisBinding>
     {
+        [SerializeField]
+        private ValueSlider _maxValueSlider;
+        [SerializeField]
+        private ValueSlider _minValueSlider;
+        [SerializeField]
+        private ValueSlider _zeroPointSlider;
+
         public override void Init(EditProfileMenu editProfileMenu, AxisBinding binding, SingleAxisBinding singleBinding)
         {
             base.Init(editProfileMenu, binding, singleBinding);
+
+            _maxValueSlider.Value = singleBinding.Maximum;
+            _minValueSlider.Value = singleBinding.Minimum;
+            _zeroPointSlider.Value = singleBinding.ZeroPoint;
+        }
+
+        public void OnMaxValueChanged(float value)
+        {
+            SingleBinding.Maximum = value;
+
+            if (value < SingleBinding.Minimum)
+                _minValueSlider.Value = value;
+
+            if (value < SingleBinding.ZeroPoint)
+                _zeroPointSlider.Value = value;
+        }
+
+        public void OnMinValueChanged(float value)
+        {
+            SingleBinding.Minimum = value;
+
+            if (value > SingleBinding.Maximum)
+                _maxValueSlider.Value = value;
+
+            if (value > SingleBinding.ZeroPoint)
+                _zeroPointSlider.Value = value;
+        }
+
+        public void OnZeroPointChanged(float value)
+        {
+            SingleBinding.ZeroPoint = value;
+
+            if (value > SingleBinding.Maximum)
+                _maxValueSlider.Value = value;
+
+            if (value < SingleBinding.Minimum)
+                _minValueSlider.Value = value;
         }
     }
 }
