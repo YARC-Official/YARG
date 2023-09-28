d6aeec279c6b99eb8e21b6dc2927894c4c4e8a25
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-09-28 16:40:41 -0400

    Added line renderers to vocal notes

--- Assets/Script/Gameplay/Visuals/VocalElements/VocalNoteElement.cs
+++ Assets/Script/Gameplay/Visuals/VocalElements/VocalNoteElement.cs
@@ -5,12 +5,30 @@ namespace YARG.Gameplay.Visuals
 {
     public class VocalNoteElement : VocalElement
     {
+        // TODO: Temporary until color profiles for vocals
+        private static readonly Color[] _colors =
+        {
+            new(0.008f, 0.710f, 0.937f, 1f),
+            new(0.667f, 0.286f, 0.012f, 1f),
+            new(0.886f, 0.561f, 0.090f, 1f)
+        };
+
         public VocalNote NoteRef { get; set; }
+
+        [SerializeField]
+        private LineRenderer[] _lineRenderers;
 
         protected override double ElementTime => NoteRef.Time;
 
         protected override void InitializeElement()
         {
+            var color = _colors[NoteRef.HarmonyPart];
+
+            foreach (var line in _lineRenderers)
+            {
+                // Set colors
+                line.material.color = color;
+            }
         }
 
         protected override void UpdateElement()
