6f34b56092265cbd6699e59a7446c267f68f204c
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-11-23 17:05:28 -0500

    Initial theme work

---
+++ Assets/Script/Themes/ThemeNote.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace YARG.Themes
+{
+    // WARNING: DO NOT CHANGE THIS!
+    // Changing the serialized fields in this file will result in older themes
+    // not working properly. Only change if you need to.
+
+    public enum ThemeNoteType
+    {
+        Normal,
+
+        HOPO,
+        Tap,
+        Open,
+        OpenHOPO,
+
+        Cymbal,
+        Kick,
+    }
+
+    public class ThemeNote : MonoBehaviour
+    {
+        [field: Space]
+        [field: SerializeField]
+        public ThemeNoteType NoteType { get; private set; }
+
+        [field: Space]
+        [field: SerializeField]
+        public MeshRenderer ColoredMaterialRenderer { get; private set; }
+
+        [field: SerializeField]
+        public int ColoredMaterialIndex { get; private set; }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(
+                transform.position.AddX(-0.4f),
+                transform.position.AddX(0.4f));
+        }
+    }
+}
