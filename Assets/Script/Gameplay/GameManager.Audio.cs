da0127c8ffc896c060da47b6f2b7719025e3c34d
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-12-11 17:40:18 -0500

    Split up GameManager file because it was getting kinda large

---
+++ Assets/Script/Gameplay/GameManager.Audio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using YARG.Helpers.Extensions;
+
+namespace YARG.Gameplay
+{
+    public partial class GameManager
+    {
+        private async UniTask LoadAudio()
+        {
+            bool isYargSong = Song.Source.Str.ToLowerInvariant() == "yarg";
+            GlobalVariables.AudioManager.Options.UseMinimumStemVolume = isYargSong;
+
+            await UniTask.RunOnThreadPool(() =>
+            {
+                try
+                {
+                    Song.LoadAudio(GlobalVariables.AudioManager, GlobalVariables.Instance.SongSpeed);
+                    SongLength = GlobalVariables.AudioManager.AudioLengthD;
+                    GlobalVariables.AudioManager.SongEnd += OnAudioEnd;
+                }
+                catch (Exception ex)
+                {
+                    _loadState = LoadFailureState.Error;
+                    _loadFailureMessage = "Failed to load audio!";
+                    Debug.LogException(ex, this);
+                }
+            });
+
+            if (_loadState != LoadFailureState.None)
+                return;
+
+            _songLoaded?.Invoke();
+        }
+
+        private void OnAudioEnd()
+        {
+            if (IsPractice)
+            {
+                PracticeManager.ResetPractice();
+                return;
+            }
+
+            if (IsReplay)
+            {
+                Pause(false);
+                return;
+            }
+
+            EndSong();
+        }
+    }
+}
---
+++ Assets/Script/Gameplay/GameManager.Loading.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using YARG.Core;
+using YARG.Core.Chart;
+using YARG.Core.Replays;
+using YARG.Gameplay.Player;
+using YARG.Menu.Navigation;
+using YARG.Menu.Persistent;
+using YARG.Playback;
+using YARG.Player;
+using YARG.Replays;
+using YARG.Settings;
+
+namespace YARG.Gameplay
+{
+    public partial class GameManager
+    {
+        private enum LoadFailureState
+        {
+            None,
+            Rescan,
+            Error
+        }
+
+        [Header("Instrument Prefabs")]
+        [SerializeField]
+        private GameObject _fiveFretGuitarPrefab;
+        [SerializeField]
+        private GameObject _sixFretGuitarPrefab;
+        [SerializeField]
+        private GameObject _fourLaneDrumsPrefab;
+        [SerializeField]
+        private GameObject _fiveLaneDrumsPrefab;
+        [SerializeField]
+        private GameObject _proGuitarPrefab;
+
+        private LoadFailureState _loadState;
+        private string _loadFailureMessage;
+
+        // All access to chart data must be done through this event,
+        // since things are loaded asynchronously
+        // Players are initialized by hand and don't go through this event
+        private event Action<SongChart> _chartLoaded;
+
+        public event Action<SongChart> ChartLoaded
+        {
+            add
+            {
+                _chartLoaded += value;
+
+                // Invoke now if already loaded, this event is only fired once
+                var chart = Chart;
+                if (chart != null) value?.Invoke(chart);
+            }
+            remove => _chartLoaded -= value;
+        }
+
+        private event Action _songLoaded;
+
+        public event Action SongLoaded
+        {
+            add
+            {
+                _songLoaded += value;
+
+                // Invoke now if already loaded, this event is only fired once
+                if (GlobalVariables.AudioManager.IsAudioLoaded) value?.Invoke();
+            }
+            remove => _songLoaded -= value;
+        }
+
+        private event Action _songStarted;
+
+        public event Action SongStarted
+        {
+            add
+            {
+                _songStarted += value;
+
+                // Invoke now if already loaded, this event is only fired once
+                if (IsSongStarted) value?.Invoke();
+            }
+            remove => _songStarted -= value;
+        }
+
+        // "The Unity message 'Start' has an incorrect signature."
+        [SuppressMessage("Type Safety", "UNT0006", Justification = "UniTaskVoid is a compatible return type.")]
+        private async UniTaskVoid Start()
+        {
+            // Disable until everything's loaded
+            enabled = false;
+
+            // Load song
+            if (IsReplay)
+            {
+                LoadingManager.Instance.Queue(LoadReplay, "Loading replay...");
+                _replayController.gameObject.SetActive(true);
+            }
+
+            LoadingManager.Instance.Queue(LoadChart, "Loading chart...");
+            LoadingManager.Instance.Queue(LoadAudio, "Loading audio...");
+            await LoadingManager.Instance.StartLoad();
+
+            if (_loadState == LoadFailureState.Rescan)
+            {
+                ToastManager.ToastWarning("Chart requires a rescan!");
+
+                GlobalVariables.Instance.LoadScene(SceneIndex.Menu);
+                return;
+            }
+
+            if (_loadState == LoadFailureState.Error)
+            {
+                Debug.LogError(_loadFailureMessage);
+                ToastManager.ToastError(_loadFailureMessage);
+
+                GlobalVariables.Instance.LoadScene(SceneIndex.Menu);
+                return;
+            }
+
+            // Initialize song runner
+            _songRunner = new SongRunner(
+                GlobalVariables.Instance.SongSpeed,
+                SettingsManager.Settings.AudioCalibration.Value,
+                SettingsManager.Settings.VideoCalibration.Value,
+                Song.SongOffsetSeconds);
+
+            // Spawn players
+            CreatePlayers();
+
+            // Listen for menu inputs
+            Navigator.Instance.NavigationEvent += OnNavigationEvent;
+
+            // Initialize/destroy practice mode
+            if (IsPractice)
+            {
+                PracticeManager.DisplayPracticeMenu();
+            }
+            else
+            {
+#if UNITY_EDITOR
+                _isShowDebugText = true;
+#endif
+
+                // Show debug info
+                _debugText.gameObject.SetActive(_isShowDebugText);
+
+                Destroy(PracticeManager);
+            }
+
+#if UNITY_EDITOR
+            // Log constant values
+            Debug.Log($"Audio calibration: {_songRunner.AudioCalibration}, video calibration: {_songRunner.VideoCalibration}, song offset: {_songRunner.SongOffset}");
+#endif
+
+            // Loaded, enable updates
+            enabled = true;
+            IsSongStarted = true;
+            _songStarted?.Invoke();
+        }
+
+        private async UniTask LoadReplay()
+        {
+            ReplayFile replayFile = null;
+            ReplayReadResult result;
+            try
+            {
+                result = await UniTask.RunOnThreadPool(() => ReplayContainer.LoadReplayFile(
+                    GlobalVariables.Instance.CurrentReplay, out replayFile));
+            }
+            catch (Exception ex)
+            {
+                _loadState = LoadFailureState.Error;
+                _loadFailureMessage = "Failed to load replay!";
+                Debug.LogException(ex, this);
+                return;
+            }
+
+            Song = GlobalVariables.Instance.SongContainer.SongsByHash[
+                GlobalVariables.Instance.CurrentReplay.SongChecksum][0];
+
+            if (Song is null || result != ReplayReadResult.Valid)
+            {
+                _loadState = LoadFailureState.Error;
+                _loadFailureMessage = "Failed to load replay!";
+                return;
+            }
+
+            Replay = replayFile.Replay;
+
+            var players = new List<YargPlayer>();
+            foreach (var frame in Replay.Frames)
+            {
+                var yargPlayer = new YargPlayer(frame.PlayerInfo.Profile, null, false);
+
+                yargPlayer.OverrideColorProfile(Replay.ColorProfiles[frame.PlayerInfo.ColorProfileId]);
+                yargPlayer.EngineParameterOverride = frame.EngineParameters;
+
+                players.Add(yargPlayer);
+            }
+
+            _yargPlayers = players;
+        }
+
+        private async UniTask LoadChart()
+        {
+            await UniTask.RunOnThreadPool(() =>
+            {
+                // Load chart
+
+                try
+                {
+                    Chart = Song.LoadChart();
+                }
+                catch (Exception ex)
+                {
+                    _loadState = LoadFailureState.Error;
+                    _loadFailureMessage = "Failed to load chart!";
+                    Debug.LogException(ex, this);
+                    return;
+                }
+
+                if (Chart is null)
+                {
+                    _loadState = LoadFailureState.Rescan;
+                    return;
+                }
+
+                // Ensure sync track is present
+                var syncTrack = Chart.SyncTrack;
+                if (syncTrack.Beatlines is null or { Count: < 1 })
+                {
+                    Chart.SyncTrack.GenerateBeatlines(Chart.GetLastTick());
+                }
+
+                // Set length of the final section
+                if (Chart.Sections.Count > 0)
+                {
+                    uint lastTick = Chart.GetLastTick();
+                    Chart.Sections[^1].TickLength = lastTick;
+                    Chart.Sections[^1].TimeLength = Chart.SyncTrack.TickToTime(lastTick);
+                }
+
+                // Autogenerate venue stuff
+                if (File.Exists(VenueAutoGenerationPreset.DefaultPath))
+                {
+                    var preset = new VenueAutoGenerationPreset(VenueAutoGenerationPreset.DefaultPath);
+                    if (Chart.VenueTrack.Lighting.Count == 0)
+                    {
+                        Debug.Log("Auto-generating venue lighting...");
+                        Chart = preset.GenerateLightingEvents(Chart);
+                    }
+
+                    // TODO: add when characters and camera events are present in game
+                    // if (Chart.VenueTrack.Camera.Count == 0)
+                    // {
+                    //     Chart = autoGenerationPreset.GenerateCameraCutEvents(Chart);
+                    // }
+                }
+            });
+
+            if (_loadState != LoadFailureState.None) return;
+
+            BeatEventHandler = new(Chart.SyncTrack);
+            _chartLoaded?.Invoke(Chart);
+        }
+
+        private void CreatePlayers()
+        {
+            _players = new List<BasePlayer>();
+
+            bool vocalTrackInitialized = false;
+
+            int index = -1;
+            foreach (var player in _yargPlayers)
+            {
+                index++;
+
+                // Skip if the player is sitting out
+                if (player.SittingOut) continue;
+
+                if (player.Profile.GameMode != GameMode.Vocals)
+                {
+                    var prefab = player.Profile.GameMode switch
+                    {
+                        GameMode.FiveFretGuitar => _fiveFretGuitarPrefab,
+                        GameMode.SixFretGuitar  => _sixFretGuitarPrefab,
+                        GameMode.FourLaneDrums  => _fourLaneDrumsPrefab,
+                        GameMode.FiveLaneDrums  => _fiveLaneDrumsPrefab,
+                        GameMode.ProGuitar      => _proGuitarPrefab,
+
+                        _ => null
+                    };
+
+                    // Skip if there's no prefab for the game mode
+                    if (prefab == null) continue;
+
+                    var playerObject = Instantiate(prefab,
+                        new Vector3(index * 100f, 100f, 0f), prefab.transform.rotation);
+
+                    // Setup player
+                    var trackPlayer = playerObject.GetComponent<TrackPlayer>();
+                    var trackView = _trackViewManager.CreateTrackView(trackPlayer, player);
+                    trackPlayer.Initialize(index, player, Chart, trackView);
+                    _players.Add(trackPlayer);
+                }
+                else
+                {
+                    // Initialize the vocal track if it hasn't been already, and hide lyric bar
+                    if (!vocalTrackInitialized)
+                    {
+                        VocalTrack.gameObject.SetActive(true);
+                        _trackViewManager.CreateVocalTrackView();
+
+                        // Since all players have to select the same vocals
+                        // type (solo/harmony) this works no problem.
+                        var chart = player.Profile.CurrentInstrument == Instrument.Vocals
+                                ? Chart.Vocals
+                                : Chart.Harmony;
+                        VocalTrack.Initialize(chart);
+
+                        _lyricBar.SetActive(false);
+                        vocalTrackInitialized = true;
+                    }
+
+                    // Create the player on the vocal track
+                    var vocalsPlayer = VocalTrack.CreatePlayer();
+                    var playerHud = _trackViewManager.CreateVocalsPlayerHUD();
+                    vocalsPlayer.Initialize(index, player, Chart, playerHud);
+                    _players.Add(vocalsPlayer);
+                }
+            }
+
+            // Make sure to set up all of the HUD positions
+            _trackViewManager.SetAllHUDPositions();
+        }
+    }
+}
