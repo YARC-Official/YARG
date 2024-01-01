f17e7cedc16c24f1eb8f8f44ded3c99b33008b48
Aaron Johnson <insomnautika@gmail.com>
2023-12-31 17:34:10 -0800

    Box Highlight Under Help Bar Buttons when Mouse Hovering

--- Assets/Script/Menu/Persistent/HelpBarButton.cs
+++ Assets/Script/Menu/Persistent/HelpBarButton.cs
@@ -1,15 +1,19 @@
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using YARG.Menu.Data;
 using YARG.Menu.Navigation;
 
 namespace YARG.Menu.Persistent
 {
-    public class HelpBarButton : MonoBehaviour
+    public class HelpBarButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
     {
         [SerializeField]
         private Image _buttonImage;
+
+        [SerializeField]
+        private Image _buttonBackground;
 
         [SerializeField]
         private TextMeshProUGUI _text;
@@ -26,11 +30,29 @@ namespace YARG.Menu.Persistent
             var icons = MenuData.NavigationIcons;
             _buttonImage.sprite = icons.GetIcon(entry.Action);
             _buttonImage.color = icons.GetColor(entry.Action);
+            _buttonBackground.color = new Color32(255, 255, 255, 0);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _buttonBackground.color = new Color32(255, 255, 255, 2);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _buttonBackground.color = new Color32(255, 255, 255, 0);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _buttonBackground.color = new Color32(255, 255, 255, 5);
         }
 
         public void OnClick()
         {
             _entry?.Invoke();
         }
+
+
     }
 }
