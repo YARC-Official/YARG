b3b6a4748489be0a66ad864269a798c527b651e9
Aaron Johnson <insomnautika@gmail.com>
2024-01-05 09:28:31 -0800

    Changed help bar hover effect to be same color as fret input.

--- Assets/Script/Menu/Persistent/HelpBarButton.cs
+++ Assets/Script/Menu/Persistent/HelpBarButton.cs
@@ -16,36 +16,54 @@ namespace YARG.Menu.Persistent
         private Image _buttonBackground;
 
         [SerializeField]
-        private TextMeshProUGUI _text;
+        private TextMeshProUGUI _buttonLabel;
+
+        [SerializeField]
+        private TextMeshProUGUI _buttonText;
 
         private NavigationScheme.Entry? _entry;
+
+        private Color32 ButtonBackgroundColor;
 
         public void SetInfoFromSchemeEntry(NavigationScheme.Entry entry)
         {
             _entry = entry;
-
-            _text.text = entry.DisplayName;
-
-            // Set the icon
             var icons = MenuData.NavigationIcons;
+            ButtonBackgroundColor = icons.GetColor(entry.Action);
+
+            _buttonLabel.text = entry.DisplayName;
+            _buttonLabel.color = Color.white;
+
+            _buttonText.color = Color.white;
+
+            // Set the icon color
             _buttonImage.sprite = icons.GetIcon(entry.Action);
-            _buttonImage.color = icons.GetColor(entry.Action);
+            _buttonImage.color = ButtonBackgroundColor;
             _buttonBackground.color = new Color32(255, 255, 255, 0);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            _buttonBackground.color = new Color32(255, 255, 255, 2);
+            _buttonBackground.color = ButtonBackgroundColor;
+            _buttonImage.color = ButtonBackgroundColor;
+            _buttonLabel.color = Color.white;
+            _buttonText.color = Color.white;
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
             _buttonBackground.color = new Color32(255, 255, 255, 0);
+            _buttonImage.color = ButtonBackgroundColor;
+            _buttonLabel.color = Color.white;
+            _buttonText.color = Color.white;
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            _buttonBackground.color = new Color32(255, 255, 255, 5);
+            _buttonBackground.color = new Color32(75, 75, 75, 255);
+            _buttonImage.color = new Color32(100, 100, 100, 255);
+            _buttonLabel.color = new Color32(100, 100, 100, 255);
+            _buttonText.color = new Color32(255, 255, 255, 25);
         }
 
         public void OnClick()
