66ae16465cf1adec5ea2ccaf8b901c52b3b62af1
Insomnautika <154550825+Insomnautika@users.noreply.github.com>
2024-01-06 12:04:14 -0800

    Box Highlight Under Help Bar Buttons when Mouse Hovering (#642)
    * Box Highlight Under Help Bar Buttons when Mouse Hovering
    
    * - Changed help bar button container position to re-align buttons with other menu items.
    
    * Changed help bar hover effect to be same color as fret input.
    
    * Cleaned up helpbar highlight code.

--- Assets/Script/Menu/Persistent/HelpBarButton.cs
+++ Assets/Script/Menu/Persistent/HelpBarButton.cs
@@ -1,36 +1,76 @@
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using YARG.Menu.Data;
 using YARG.Menu.Navigation;
 
 namespace YARG.Menu.Persistent
 {
-    public class HelpBarButton : MonoBehaviour
+    public class HelpBarButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
     {
         [SerializeField]
         private Image _buttonImage;
 
         [SerializeField]
-        private TextMeshProUGUI _text;
+        private Image _buttonBackground;
+
+        [SerializeField]
+        private TextMeshProUGUI _buttonLabel;
+
+        [SerializeField]
+        private TextMeshProUGUI _buttonText;
 
         private NavigationScheme.Entry? _entry;
+
+        private Color _buttonBackgroundColor;
 
         public void SetInfoFromSchemeEntry(NavigationScheme.Entry entry)
         {
             _entry = entry;
-
-            _text.text = entry.DisplayName;
-
-            // Set the icon
             var icons = MenuData.NavigationIcons;
+            _buttonBackgroundColor = icons.GetColor(entry.Action);
+
+            _buttonLabel.text = entry.DisplayName;
+            _buttonLabel.color = Color.white;
+
+            _buttonText.color = Color.white;
+
+            // Set the icon color
             _buttonImage.sprite = icons.GetIcon(entry.Action);
-            _buttonImage.color = icons.GetColor(entry.Action);
+            _buttonImage.color = _buttonBackgroundColor;
+            _buttonBackground.color = Color.clear;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _buttonBackground.color = _buttonBackgroundColor;
+            _buttonImage.color = _buttonBackgroundColor;
+            _buttonLabel.color = Color.white;
+            _buttonText.color = Color.white;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _buttonBackground.color = Color.clear;
+            _buttonImage.color = _buttonBackgroundColor;
+            _buttonLabel.color = Color.white;
+            _buttonText.color = Color.white;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _buttonBackground.color = Color.grey;
+            _buttonImage.color = Color.grey;
+            _buttonLabel.color = Color.grey;
+            _buttonText.color = Color.grey;
         }
 
         public void OnClick()
         {
             _entry?.Invoke();
         }
+
+
     }
 }
