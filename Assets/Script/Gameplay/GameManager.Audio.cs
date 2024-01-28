29b760ad99f6754fa6280f572830f716c10f347a
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-01-28 06:19:09 +0000

    Change reverb state function

--- Assets/Script/Gameplay/GameManager.Audio.cs
+++ Assets/Script/Gameplay/GameManager.Audio.cs
@@ -10,10 +10,11 @@ namespace YARG.Gameplay
 {
     public partial class GameManager
     {
-        public class StemState
+        public struct StemState
         {
             public int Total;
             public int Muted;
+            public int ReverbCount;
 
             public float GetVolumeLevel()
             {
@@ -32,6 +33,8 @@ namespace YARG.Gameplay
         {
             // The stem states are initialized in "CreatePlayers"
             _stemStates.Clear();
+            _stemStates.Add(SongStem.Keys, new StemState
+                { Total = 1});
 
             bool isYargSong = Song.Source.Str.ToLowerInvariant() == "yarg";
             GlobalVariables.AudioManager.Options.UseMinimumStemVolume = isYargSong;
@@ -61,7 +64,7 @@ namespace YARG.Gameplay
         {
             if (!SettingsManager.Settings.MuteOnMiss.Value) return;
 
-            if (!_stemStates.TryGetValue(stem, out var state)) return;
+            if (!GlobalVariables.AudioManager.HasStem(stem) || !_stemStates.TryGetValue(stem, out var state)) return;
 
             if (muted)
             {
@@ -83,6 +86,33 @@ namespace YARG.Gameplay
                 GlobalVariables.AudioManager.SetStemVolume(SongStem.Drums2, volume);
                 GlobalVariables.AudioManager.SetStemVolume(SongStem.Drums3, volume);
                 GlobalVariables.AudioManager.SetStemVolume(SongStem.Drums4, volume);
+            }
+        }
+
+        public void ChangeStemReverbState(SongStem stem, bool reverb)
+        {
+            if (!SettingsManager.Settings.UseStarpowerFx.Value) return;
+
+            if (!GlobalVariables.AudioManager.HasStem(stem) || !_stemStates.TryGetValue(stem, out var state)) return;
+
+            if (reverb)
+            {
+                state.ReverbCount++;
+            }
+            else
+            {
+                state.ReverbCount--;
+            }
+
+            if(state.ReverbCount > 0)
+            {
+                GlobalVariables.AudioManager.ApplyReverb(stem, true);
+                EditorDebug.Log($"Applied reverb to {stem}");
+            }
+            else
+            {
+                GlobalVariables.AudioManager.ApplyReverb(stem, false);
+                EditorDebug.Log($"Removed reverb from {stem}");
             }
         }
 
