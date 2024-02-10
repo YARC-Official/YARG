6eb6993bae1759b88ac504bc683db9e0b17b604f
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2024-02-10 13:43:09 -0800

    Refactored ToggleGroup for ColoredButtonGroup

---
+++ Assets/Script/Menu/Common/Buttons/ColoredButtonGroup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using YARG.Menu;
+using YARG.Menu.Data;
+
+namespace YARG
+{
+    public class ColoredButtonGroup : MonoBehaviour
+    {
+        [SerializeField]
+        private List<ColoredButton> _buttons;
+
+        public delegate UnityAction OnButtonClicked();
+        public OnButtonClicked ClickedButton;
+
+        public ColoredButton ActiveButton { get; private set; }
+
+        private ColoredButton _prevActivatedButton;
+
+        public void OnEnable()
+        {
+            foreach (var button in _buttons)
+            {
+                button.OnClick.RemoveAllListeners();
+                button.OnClick.AddListener(() => OnClick(button));
+            }
+        }
+
+        private void OnClick(ColoredButton button)
+        {
+            if (_prevActivatedButton == null)
+            {
+                _prevActivatedButton = button;
+            }
+            else
+            {
+                if (ActiveButton != null)
+                {
+                    _prevActivatedButton = ActiveButton;
+                    _prevActivatedButton.SetBackgroundAndTextColor(MenuData.Colors.DeactivatedButton);
+                }
+            }
+
+            if (ActiveButton == null || ActiveButton != button)
+            {
+                ActiveButton = button;
+                ActiveButton.SetBackgroundAndTextColor(MenuData.Colors.BrightButton);
+            }
+            else
+            {
+                ActiveButton.SetBackgroundAndTextColor(MenuData.Colors.DeactivatedButton);
+                ActiveButton = null;
+            }
+
+            ClickedButton?.Invoke();
+        }
+    }
+}
