732dfa1df1c9383f50e6fabaa8b16b2b5568426c
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-03-12 22:38:58 +0000

    Overwrite internal unity fields with custom logger

---
+++ Assets/Script/Persistent/Logging/Unity/UnityInternalLogWrapper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace YARG.Logging.Unity
+{
+    public static class UnityInternalLogWrapper
+    {
+        // Reference: https://github.com/Unity-Technologies/UnityCsReference/blob/2021.3/Runtime/Export/Debug/Debug.bindings.cs
+
+        public static UnityInternalLog UnityInternalLogDelegate { get; private set; }
+
+        public static UnityInternalLogException UnityInternalLogExceptionDelegate { get; private set; }
+
+        private static FieldInfo s_LoggerField;
+
+        private static ILogger _originalUnityLogger;
+
+        public static void OverwriteUnityInternals()
+        {
+            const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Static;
+
+            var debugLogHandler = typeof(Debug).Assembly.GetType("UnityEngine.DebugLogHandler");
+            var logMethod = debugLogHandler.GetMethod("Internal_Log", flags);
+            var logExceptionMethod = debugLogHandler.GetMethod("Internal_LogException", flags);
+
+            var deleg = logMethod.CreateDelegate(typeof(UnityInternalLog));
+            var delegException = logExceptionMethod.CreateDelegate(typeof(UnityInternalLogException));
+
+            UnityInternalLogDelegate = (UnityInternalLog) deleg;
+            UnityInternalLogExceptionDelegate = (UnityInternalLogException) delegException;
+
+            // Override internal Debug.s_Logger to our own
+            var debugType = typeof(Debug);
+            s_LoggerField = debugType.GetField("s_Logger", flags);
+
+            _originalUnityLogger = (ILogger) s_LoggerField.GetValue(null);
+
+            // We pass in the original ILogHandler to our custom logger because some parts of Unity
+            // explicitly check if its their type (DebugLogHandler) for certain exception logging
+            s_LoggerField.SetValue(null, new CustomUnityLogger(_originalUnityLogger.logHandler));
+        }
+
+        public static void RestoreUnityInternals()
+        {
+            var debugType = typeof(Debug);
+            var loggerField = s_LoggerField ??
+                debugType.GetField("s_Logger", BindingFlags.NonPublic | BindingFlags.Static);
+
+            // Restore original Debug.s_Logger
+            if (_originalUnityLogger is not null)
+            {
+                loggerField.SetValue(null, _originalUnityLogger);
+            }
+            else
+            {
+                var loggerType = debugType.Assembly.GetType("UnityEngine.Logger");
+                var loggerHandlerType = debugType.Assembly.GetType("UnityEngine.DebugLogHandler");
+
+                var handler = Activator.CreateInstance(loggerHandlerType);
+                var logger = Activator.CreateInstance(loggerType, handler);
+
+                loggerField.SetValue(null, logger);
+            }
+        }
+
+        public delegate void UnityInternalLog(LogType level, LogOption options, string msg, Object obj);
+
+        public delegate void UnityInternalLogException(Exception ex, Object obj);
+    }
+}
