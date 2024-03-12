d2546c2a9a0c8b651b3251efcaa77526659ba6eb
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-03-12 14:56:27 +0000

    Write to internal unity logger

---
+++ Assets/Script/Persistent/Logging/Unity/UnityEditorLogFormat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Cysharp.Text;
+using YARG.Core.Logging;
+
+namespace YARG
+{
+    public class UnityEditorLogFormat : IYargLogFormatter
+    {
+        public void FormatLogItem(ref Utf16ValueStringBuilder output, LogItem item)
+        {
+            var source = item.Source.AsSpan();
+            var separator = Path.DirectorySeparatorChar;
+
+            int lastSeparatorIndex = source.LastIndexOf(separator);
+            var fileName = source[(lastSeparatorIndex + 1)..];
+
+            output.Append("[");
+
+            // Append File
+            output.Append(fileName);
+
+            // Append :Method:Line
+            output.AppendFormat(":{0}:{1}] ", item.Method, item.Line);
+
+            if (!string.IsNullOrEmpty(item.Message))
+            {
+                output.Append(item.Message);
+            }
+            else if (!string.IsNullOrEmpty(item.Format))
+            {
+                var argCount = Array.IndexOf(item.Args, null);
+
+                for(int i = argCount; i < item.Args.Length; i++)
+                {
+                    item.Args[i] = null;
+                }
+
+                output.AppendFormat(item.Format, item.Args[0], item.Args[1], item.Args[2], item.Args[3], item.Args[4],
+                    item.Args[5], item.Args[6], item.Args[7], item.Args[8], item.Args[9]);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Persistent/Logging/Unity/UnityEditorLogListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Cysharp.Text;
+using UnityEngine;
+using YARG.Core.Logging;
+using Object = UnityEngine.Object;
+
+namespace YARG
+{
+    public class UnityEditorLogListener : BaseYargLogListener
+    {
+        private readonly UnityInternalLog _unityInternalLog;
+        private readonly UnityInternalLogException _unityInternalLogException;
+
+        // Reference: https://github.com/Unity-Technologies/UnityCsReference/blob/2021.3/Runtime/Export/Debug/Debug.bindings.cs
+
+        public UnityEditorLogListener(IYargLogFormatter formatter) : base(formatter)
+        {
+            const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Static;
+
+            var debugLogHandler = typeof(Debug).Assembly.GetType("UnityEngine.DebugLogHandler");
+            var logMethod = debugLogHandler.GetMethod("Internal_Log", flags);
+            var logExceptionMethod = debugLogHandler.GetMethod("Internal_LogException", flags);
+
+            var deleg = logMethod.CreateDelegate(typeof(UnityInternalLog));
+            var delegException = logExceptionMethod.CreateDelegate(typeof(UnityInternalLogException));
+
+            //Debug.Log();
+
+            _unityInternalLog = (UnityInternalLog) deleg;
+            _unityInternalLogException = (UnityInternalLogException) delegException;
+        }
+
+        public override void WriteLogItem(ref Utf16ValueStringBuilder builder)
+        {
+            // LogType level,
+            // LogOption options,
+            // string msg,
+            // Object obj
+            _unityInternalLog(LogType.Log, LogOption.NoStacktrace, builder.ToString(), null);
+            _unityInternalLogException(new Exception("hi"), null);
+        }
+    }
+
+    public delegate void UnityInternalLog(LogType level, LogOption options, string msg, Object obj);
+    public delegate void UnityInternalLogException(Exception ex, Object obj);
+}
