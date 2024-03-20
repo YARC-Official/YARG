bffd63855e1222c7ee43b90a9fcd4ea4d8ee0344
Vinay Kapadia <vinaykapadia@users.noreply.github.com>
2024-03-20 15:01:25 -0700

    YARG-209 Show Bad Songs count on Scan (#711)
    * YARG-209 Show Bad Songs count on Scan
    
    If there is a bad songs file, a button will be shown to open the file. On a scan, the number of errors will be shown on the button.
    
    * Fixing button visuals.
    
    ---------
    
    Co-authored-by: Vinay Kapadia <dev@vkapadia.com>

--- Assets/Script/Menu/Settings/SongManager/SongManagerHeader.cs
+++ Assets/Script/Menu/Settings/SongManager/SongManagerHeader.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
+using YARG.Core.Song.Cache;
+using YARG.Helpers;
 using YARG.Settings;
 
 namespace YARG.Menu.Settings
 {
     public class SongManagerHeader : MonoBehaviour
     {
+        [SerializeField]
+        private ColoredButton _badSongsButton;
+
+        private void Awake()
+        {
+            CheckBadSongsFile();
+        }
+
         public void AddNewFolder()
         {
             SettingsManager.Settings.SongFolders.Add(string.Empty);
@@ -16,6 +27,24 @@ namespace YARG.Menu.Settings
             LoadingManager.Instance.QueueSongRefresh(false);
             await LoadingManager.Instance.StartLoad();
             SettingsMenu.Instance.RefreshAndKeepPosition();
+        }
+
+        private void CheckBadSongsFile()
+        {
+            _badSongsButton.gameObject.SetActive(File.Exists(PathHelper.BadSongsPath));
+            
+            var numErrors = CacheHandler.Progress.BadSongCount;
+
+            if (numErrors > 0)
+            {
+                var errors = numErrors == 1 ? "ERROR" : "ERRORS";
+                _badSongsButton.Text.text = $"{numErrors} {errors} FOUND";
+            }
+        }
+
+        public void OpenBadSongs()
+        {
+            FileExplorerHelper.OpenFolder(PathHelper.BadSongsPath);
         }
     }
 }
