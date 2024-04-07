f61f9e95fe8708a32e253344cf782cfe14236f53
TheFatBastid <tfb@sof.ws>
2024-04-07 00:36:37 -0400

    Fixed errant duplicate Add bug
    Event handlers aren't added/removed in the order requested so having two lists that first do Add then Remove causes anomalies. Changing to one list fixes that.

--- Assets/Script/Playback/BeatEventHandler.cs
+++ Assets/Script/Playback/BeatEventHandler.cs
@@ -94,15 +94,13 @@ namespace YARG.Playback
             {
                 // Apply offset up-front
                 songTime -= _offset;
-                if (songTime < 0)
-                    return;
+                if (songTime < 0) return;
 
                 var tempos = sync.Tempos;
                 var timeSigs = sync.TimeSignatures;
 
                 // Progress tempo map
-                while (_tempoIndex + 1 < tempos.Count && tempos[_tempoIndex + 1].Time < songTime)
-                    _tempoIndex++;
+                while (_tempoIndex + 1 < tempos.Count && tempos[_tempoIndex + 1].Time < songTime) _tempoIndex++;
 
                 while (_timeSigIndex + 1 < timeSigs.Count && timeSigs[_timeSigIndex + 1].Time < songTime)
                 {
@@ -153,8 +151,7 @@ namespace YARG.Playback
             {
                 // Apply offset up-front
                 songTime -= _offset;
-                if (songTime < 0)
-                    return;
+                if (songTime < 0) return;
 
                 var beatlines = sync.Beatlines;
                 while (_beatlineIndex + 1 < beatlines.Count && beatlines[_beatlineIndex + 1].Time < songTime)
@@ -175,8 +172,9 @@ namespace YARG.Playback
         private readonly Dictionary<Delegate, IBeatAction> _states = new();
 
         // Necessary to allow adding or removing subscriptions within event handlers
-        private readonly List<(Delegate, IBeatAction)> _addStates = new();
-        private readonly List<Delegate> _removeStates = new();
+        private readonly List<(bool, Delegate, IBeatAction)> _changeStates = new();
+        //private readonly List<(Delegate, IBeatAction)> _addStates = new();
+        //private readonly List<Delegate> _removeStates = new();
 
         public BeatEventHandler(SyncTrack sync)
         {
@@ -193,7 +191,8 @@ namespace YARG.Playback
         public void Subscribe(Action action, float beatRate, double offset = 0,
             TempoMapEventMode mode = TempoMapEventMode.Beat)
         {
-            _addStates.Add((action, new TempoMapAction(action, beatRate, offset, mode)));
+            _changeStates.Add((true, action, new TempoMapAction(action, beatRate, offset, mode)));
+            //_addStates.Add((action, new TempoMapAction(action, beatRate, offset, mode)));
         }
 
         /// <summary>
@@ -203,17 +202,20 @@ namespace YARG.Playback
         /// <param name="offset">The constant offset to use for the event.</param>
         public void Subscribe(Action<Beatline> action, double offset = 0)
         {
-            _addStates.Add((action, new BeatlineAction(action, offset)));
+            _changeStates.Add((true, action, new BeatlineAction(action, offset)));
+            //_addStates.Add((action, new BeatlineAction(action, offset)));
         }
 
         public void Unsubscribe(Action action)
         {
-            _removeStates.Add(action);
+            _changeStates.Add((false, action, null));
+            //_removeStates.Add(action);
         }
 
         public void Unsubscribe(Action<Beatline> action)
         {
-            _removeStates.Add(action);
+            _changeStates.Add((false, action, null));
+            //_removeStates.Add(action);
         }
 
         public void ResetTimers()
@@ -229,22 +231,22 @@ namespace YARG.Playback
 
         public void Update(double songTime)
         {
-            // Add/remove new subscriptions
-            foreach (var action in _removeStates)
+            foreach (var (add, action, state) in _changeStates)
             {
-                _states.Remove(action);
-            }
-
-            foreach (var (action, state) in _addStates)
-            {
-                if (!_states.TryAdd(action, state))
+                if (add)
                 {
-                    YargLogger.LogWarning("A beat event handler with the same action has already been added!");
+                    if (!_states.TryAdd(action, state))
+                    {
+                        YargLogger.LogWarning("A beat event handler with the same action has already been added!");
+                    }
+                }
+                else
+                {
+                    _states.Remove(action);
                 }
             }
 
-            _removeStates.Clear();
-            _addStates.Clear();
+            _changeStates.Clear();
 
             // Skip until in the chart
             if (songTime < 0) return;
