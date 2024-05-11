a42fa8153b09a2363358811b4220f8318273a726
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-05-12 00:24:18 +0100

    Vector2 Setting type

---
+++ Assets/Script/Settings/Types/Vector2Setting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace YARG.Settings.Types
+{
+    public class Vector2Setting : AbstractSetting<Vector2>
+    {
+        public Vector2Setting(Action<Vector2> onChange) : base(onChange)
+        {
+        }
+
+        public override string AddressableName => "Setting/Vector2";
+
+        public override bool ValueEquals(Vector2 value) => value == Value;
+    }
+}
