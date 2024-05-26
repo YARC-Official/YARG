b748fffd4be537a48c93b7c1b1fa845e5331d207
Xilophor <ryanzgit@pm.me>
2024-05-26 18:34:31 -0400

    Remove unnecessary using

--- Assets/Script/Song/SongSearching.cs
+++ Assets/Script/Song/SongSearching.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 using FuzzySharp;
 using YARG.Core;
 using YARG.Core.Song;
