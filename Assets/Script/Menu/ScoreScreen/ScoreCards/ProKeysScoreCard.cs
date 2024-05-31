baf1d4149a65cd846f99237e7d421d02908a9757
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-05-31 17:35:09 -0400

    Added pro keys score card

---
+++ Assets/Script/Menu/ScoreScreen/ScoreCards/ProKeysScoreCard.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using YARG.Core.Engine.ProKeys;
+
+namespace YARG.Menu.ScoreScreen
+{
+    public class ProKeysScoreCard : ScoreCard<ProKeysStats>
+    {
+        [Space]
+        [SerializeField]
+        private TextMeshProUGUI _overhits;
+
+        public override void SetCardContents()
+        {
+            base.SetCardContents();
+
+            _overhits.text = WrapWithColor(Stats.Overhits);
+        }
+    }
+}
--- Assets/Script/Menu/ScoreScreen/ScoreScreenMenu.cs
+++ Assets/Script/Menu/ScoreScreen/ScoreScreenMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using YARG.Core;
 using YARG.Core.Engine.Drums;
 using YARG.Core.Engine.Guitar;
+using YARG.Core.Engine.ProKeys;
 using YARG.Core.Engine.Vocals;
 using YARG.Core.Input;
 using YARG.Core.Logging;
@@ -39,6 +40,8 @@ namespace YARG.Menu.ScoreScreen
         private DrumsScoreCard _drumsCardPrefab;
         [SerializeField]
         private VocalsScoreCard _vocalsCardPrefab;
+        [SerializeField]
+        private ProKeysScoreCard _proKeysCardPrefab;
 
         // "The Unity message 'OnEnable' has an incorrect signature."
         [SuppressMessage("Type Safety", "UNT0006", Justification = "UniTaskVoid is a compatible return type.")]
@@ -117,6 +120,13 @@ namespace YARG.Menu.ScoreScreen
                     {
                         var card = Instantiate(_vocalsCardPrefab, _cardContainer);
                         card.Initialize(score.Player, score.Stats as VocalsStats);
+                        card.SetCardContents();
+                        break;
+                    }
+                    case GameMode.ProKeys:
+                    {
+                        var card = Instantiate(_proKeysCardPrefab, _cardContainer);
+                        card.Initialize(score.Player, score.Stats as ProKeysStats);
                         card.SetCardContents();
                         break;
                     }
