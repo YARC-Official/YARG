1a4637a22d42286cf1ecb56d91a1293cdf3ab1cd
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-06-01 16:04:40 -0400

    Added in chord bar ends

--- Assets/Script/Gameplay/Visuals/TrackElements/ProKeys/ProKeysChordBarElement.cs
+++ Assets/Script/Gameplay/Visuals/TrackElements/ProKeys/ProKeysChordBarElement.cs
@@ -12,7 +12,17 @@ namespace YARG.Gameplay.Visuals
         public override double ElementTime => NoteRef.Time;
 
         [SerializeField]
+        private float _middlePadding;
+        [SerializeField]
+        private float _endOffsets;
+
+        [Space]
+        [SerializeField]
         private Transform _middleModel;
+        [SerializeField]
+        private Transform _leftModel;
+        [SerializeField]
+        private Transform _rightModel;
 
         protected override void InitializeElement()
         {
@@ -32,15 +42,20 @@ namespace YARG.Gameplay.Visuals
                 }
             }
 
-            var minPos = Player.GetNoteX(min!.Value);
-            var maxPos = Player.GetNoteX(max!.Value);
+            var minPos = Player.GetNoteX(min!.Value) - _middlePadding;
+            var maxPos = Player.GetNoteX(max!.Value) + _middlePadding;
 
             var size = maxPos - minPos;
             var mid = (minPos + maxPos) / 2f;
 
+            // Transform the middle model
             var cachedTransform = _middleModel.transform;
             cachedTransform.localScale = new Vector3(size, 1f, 1f);
             cachedTransform.localPosition = new Vector3(mid, 0f, 0f);
+
+            // Transform the end models
+            _leftModel.localPosition = _leftModel.localPosition.WithX(minPos - _endOffsets);
+            _rightModel.localPosition = _rightModel.localPosition.WithX(maxPos + _endOffsets);
         }
 
         protected override void UpdateElement()
