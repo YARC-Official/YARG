175b1a84a0b304ddcc45b2f68df537fd087bce0f
RileyTheFox <jacobchambers@hotmail.co.uk>
2024-06-11 14:24:37 +0100

    Check Resources directory exists

--- Assets/Editor/Build/BuildGitCommitVersion.cs
+++ Assets/Editor/Build/BuildGitCommitVersion.cs
@@ -7,9 +7,16 @@ namespace Editor.Build
 {
     public class BuildGitCommitVersion : IPreprocessBuildWithReport
     {
+        private const string OUTPUT_FOLDER = "Assets/Resources";
+
         public int callbackOrder { get; }
         public void OnPreprocessBuild(BuildReport report)
         {
+            if (!Directory.Exists(OUTPUT_FOLDER))
+            {
+                Directory.CreateDirectory(OUTPUT_FOLDER);
+            }
+
             File.WriteAllText("Assets/Resources/version.txt", GlobalVariables.LoadVersionFromGit());
         }
     }
