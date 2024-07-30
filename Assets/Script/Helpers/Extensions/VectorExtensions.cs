ee2448c9d9af4047fe41a3294a8f98c34eb4f86a
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-07-30 16:16:09 -0400

    Removed unused struct

--- Assets/Script/Helpers/Extensions/VectorExtensions.cs
+++ Assets/Script/Helpers/Extensions/VectorExtensions.cs
@@ -292,26 +292,4 @@ namespace UnityEngine
         }
         #endregion
     }
-
-    public struct SerializableVector2
-    {
-        public float x;
-        public float y;
-
-        public SerializableVector2(float x, float y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public static implicit operator Vector2(SerializableVector2 v)
-        {
-            return new Vector2(v.x, v.y);
-        }
-
-        public static implicit operator SerializableVector2(Vector2 v)
-        {
-            return new SerializableVector2(v.x, v.y);
-        }
-    }
 }
