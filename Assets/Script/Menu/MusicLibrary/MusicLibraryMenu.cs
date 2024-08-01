8a654d3e5f39fc7036ed4509d2019d860f527ef1
Purplo <33099317+Purplo-cf@users.noreply.github.com>
2024-08-01 13:43:22 -0700

    Library popup hotfix (#839)
    * Hotfix: Allow changing library sort when only one sort header is present, prevent *Go To* instead when there aren't enough headers
    
    * Simplify HasSortHeaders logic

--- Assets/Script/Menu/MusicLibrary/MusicLibraryMenu.cs
+++ Assets/Script/Menu/MusicLibrary/MusicLibraryMenu.cs
@@ -216,7 +216,8 @@ namespace YARG.Menu.MusicLibrary
 
             var viewList = (SelectedPlaylist is not null) ? CreatePlaylistViewList() : CreateNormalViewList();
 
-            HasSortHeaders = viewList.Any(x => x is SortHeaderViewType);
+            // Disable shortcuts if there are less than 2 sort headers in the viewlist
+            HasSortHeaders = _sortedSongs is not null && _sortedSongs.Length > 1;
 
             return viewList;
         }
