b83d2ae814befa7b7fa1e6fe0b9503f8bf6ec5db
Nathan <nathanhurst03@gmail.com>
2024-08-17 07:23:45 -0600

    Restore commented-out state/stats display and add remaining state/stats

--- Assets/Script/Gameplay/GameManager.Debug.cs
+++ Assets/Script/Gameplay/GameManager.Debug.cs
@@ -113,6 +113,8 @@ namespace YARG.Gameplay
         private Vector2 _debugBaseEngineScroll;
         private Vector2 _debugDerivedEngineScroll;
 
+        private bool _debugProKeysPressTimesToggle;
+
         private void PlayerDebug()
         {
             GUILayout.BeginVertical("Player Selection", VerticalGroupStyle);
@@ -131,7 +133,7 @@ namespace YARG.Gameplay
                 using var text = ZString.CreateStringBuilder(true);
 
                 var engine = player.BaseEngine;
-                text.AppendLine("State:");
+                text.AppendLine("Time state:");
                 text.AppendFormat("- Current tick: {0}\n", engine.CurrentTick);
                 text.AppendFormat("- Current time: {0:0.000000}\n", engine.CurrentTime);
                 text.AppendFormat("- Last tick: {0}\n", engine.LastTick);
@@ -141,43 +143,70 @@ namespace YARG.Gameplay
                 else
                     text.Append("- Last queued input time: None\n");
                 text.AppendLine();
+                text.AppendLine("Indexes:");
                 text.AppendFormat("- Note index: {0}\n", engine.NoteIndex);
                 text.AppendFormat("- Solo index: {0}\n", engine.CurrentSoloIndex);
                 text.AppendFormat("- Star index: {0}\n", engine.CurrentStarIndex);
                 text.AppendFormat("- Countdown index: {0}\n", engine.CurrentWaitCountdownIndex);
                 text.AppendLine();
+                text.AppendLine("Star Power:");
+                text.AppendFormat("- Ticks per quarter bar: {0}\n", engine.TicksPerQuarterSpBar);
+                text.AppendFormat("- Ticks per half bar: {0}\n", engine.TicksPerHalfSpBar);
+                text.AppendFormat("- Ticks per full bar: {0}\n", engine.TicksPerFullSpBar);
+                text.AppendLine();
+                text.AppendFormat("- Current Star Power tick: {0}\n", engine.StarPowerTickPosition);
+                text.AppendFormat("- Last Star Power tick: {0}\n", engine.PreviousStarPowerTickPosition);
+                text.AppendFormat("- Activation start tick: {0}\n", engine.StarPowerTickActivationPosition);
+                text.AppendFormat("- Activation end tick: {0}\n", engine.StarPowerTickEndPosition);
+                text.AppendFormat("- Activation start time: {0:0.000000}\n", engine.StarPowerActivationTime);
+                text.AppendFormat("- Activation end time: {0:0.000000}\n", engine.StarPowerEndTime);
+                text.AppendLine();
+                // text.AppendFormat("- Current drain rate: {0}\n", ); // TODO?
+                text.AppendFormat("- Last whammy tick: {0}\n", engine.LastStarPowerWhammyTick);
+                text.AppendFormat("- Star Power whammy timer:\n     {0}\n", engine.GetStarPowerWhammyTimer());
+                text.AppendLine();
+                text.AppendLine("Miscellaneous:");
                 text.AppendFormat("- Solo active: {0}\n", engine.IsSoloActive);
                 text.AppendFormat("- Wait countdown active: {0}\n", engine.IsWaitCountdownActive);
                 text.AppendFormat("- Star Power input active: {0}\n", engine.IsStarPowerInputActive);
+                text.AppendFormat("- Can activate Star Power: {0}\n", engine.CanStarPowerActivate);
 
                 var stats = player.BaseEngine.BaseStats;
-                text.AppendLine("\nStats:");
+                text.AppendLine("\nScore stats:");
+                text.AppendFormat("- Base score: {0}\n", engine.BaseScore);
                 text.AppendFormat("- Committed score: {0}\n", stats.CommittedScore);
                 text.AppendFormat("- Pending score: {0}\n", stats.PendingScore);
                 text.AppendFormat("- Total score: {0}\n", stats.TotalScore);
                 text.AppendFormat("- Star score: {0}\n", stats.StarScore);
                 text.AppendFormat("- Stars: {0}\n", stats.Stars);
                 text.AppendLine();
-                text.AppendFormat("- Solo bonus score: {0}\n", stats.SoloBonuses);
+                text.AppendFormat("- Sustain score: {0}\n", stats.SustainScore);
                 text.AppendFormat("- Star Power score: {0}\n", stats.StarPowerScore);
+                text.AppendFormat("- Solo bonus score: {0}\n", stats.SoloBonuses);
                 text.AppendLine();
+                text.AppendLine("Combo stats:");
                 text.AppendFormat("- Combo: {0}\n", stats.Combo);
                 text.AppendFormat("- Max combo: {0}\n", stats.MaxCombo);
                 text.AppendFormat("- Multiplier: {0}\n", stats.ScoreMultiplier);
                 text.AppendLine();
                 text.AppendFormat("- Notes hit: {0}/{1}\n", stats.NotesHit, stats.TotalNotes);
                 text.AppendFormat("- Notes missed: {0}\n", stats.NotesMissed);
-                text.AppendFormat("- Note hit percentage: {0}\n", stats.Percent);
+                text.AppendFormat("- Note hit percentage: {0:0.000}\n", stats.Percent);
                 text.AppendLine();
+                text.AppendLine("Star Power stats:");
                 text.AppendFormat("- Star Power phrases hit: {0}/{1}\n",
                     stats.StarPowerPhrasesHit, stats.TotalStarPowerPhrases);
                 text.AppendFormat("- Star Power phrases missed: {0}\n", stats.StarPowerPhrasesMissed);
                 text.AppendLine();
                 text.AppendFormat("- Star Power active: {0}\n", stats.IsStarPowerActive);
-                text.AppendFormat("- Star Power bar amount: {0:0.000000}\n", stats.StarPowerBarAmount);
+                text.AppendFormat("- Star Power bar amount: {0:0.000000}\n", engine.GetStarPowerBarAmount());
                 text.AppendFormat("- Star Power tick amount: {0}\n", stats.StarPowerTickAmount);
                 text.AppendFormat("- Total Star Power ticks: {0}\n", stats.TotalStarPowerTicks);
-                text.AppendFormat("- Time in Star Power: {0:0.000000}\n", stats.TimeInStarPower);
+                text.AppendFormat("- Star Power whammy ticks: {0}\n", stats.StarPowerWhammyTicks);
+                text.AppendLine();
+                text.AppendFormat("- Star Power activation count: {0}\n", stats.StarPowerActivationCount);
+                text.AppendFormat("- Total Star Power bars filled: {0:0.000000}\n", stats.TotalStarPowerBarsFilled);
+                text.AppendFormat("- Total time in Star Power: {0:0.000000}\n", stats.TimeInStarPower);
 
                 GUILayout.Label(text.AsSpan().TrimEnd('\n').ToString());
             }
@@ -204,16 +233,16 @@ namespace YARG.Gameplay
                         var engine = fiveFretPlayer.Engine;
                         text.AppendLine("State:");
                         text.AppendFormat("- Button mask: 0x{0:X2}\n", engine.ButtonMask);
-                        // text.AppendFormat("- Last button mask: 0x{0:X2}\n", engine.LastButtonMask);
-                        // text.AppendFormat("- Note was ghosted: {0}\n", engine.WasNoteGhosted);
+                        text.AppendFormat("- Last button mask: 0x{0:X2}\n", engine.LastButtonMask);
+                        text.AppendFormat("- Note was ghosted: {0}\n", engine.WasNoteGhosted);
                         text.AppendLine();
-                        // text.AppendFormat("- Strum leniency timer: {0}\n", engine.HopoLeniencyTimer);
-                        // text.AppendFormat("- HOPO leniency timer: {0}\n", engine.StrumLeniencyTimer);
-                        text.AppendFormat("- Star Power whammy timer: {0}\n", engine.StarPowerWhammyTimer);
-                        // if (engine.FrontEndExpireTime != double.MaxValue)
-                        //     text.AppendFormat("- Front-end expire time: {0:0.000000}\n", engine.FrontEndExpireTime);
-                        // else
-                        //     text.Append("- Front-end expire time: Not set\n");
+                        text.AppendFormat("- Strum leniency timer: {0}\n", engine.GetHopoLeniencyTimer());
+                        text.AppendFormat("- HOPO leniency timer: {0}\n", engine.GetStrumLeniencyTimer());
+                        double frontEndExpire = engine.GetFrontEndExpireTime();
+                        if (frontEndExpire != double.MaxValue)
+                            text.AppendFormat("- Front-end expire time: {0:0.000000}\n", frontEndExpire);
+                        else
+                            text.Append("- Front-end expire time: Not set\n");
 
                         var stats = fiveFretPlayer.Engine.EngineStats;
                         text.AppendLine("\nStats:");
@@ -221,7 +250,6 @@ namespace YARG.Gameplay
                         text.AppendFormat("- Ghost inputs: {0}\n", stats.GhostInputs);
                         text.AppendFormat("- HOPOs strummed: {0}\n", stats.HoposStrummed);
                         text.AppendFormat("- Sustain score: {0}\n", stats.SustainScore);
-                        text.AppendFormat("- Star Power whammy ticks: {0}\n", stats.WhammyTicks);
 
                         GUILayout.Label(text.AsSpan().TrimEnd('\n').ToString());
                         break;
@@ -270,11 +298,28 @@ namespace YARG.Gameplay
 
                         var engine = proKeysPlayer.Engine;
                         text.AppendLine("State:");
-                        // text.AppendFormat("- Key mask: 0x{0:X8}\n", engine.KeyMask);
-                        // text.AppendFormat("- Previous key mask: 0x{0:X8}\n", engine.PreviousKeyMask);
+                        text.AppendFormat("- Key mask: 0x{0:X8}\n", engine.KeyMask);
+                        text.AppendFormat("- Previous key mask: 0x{0:X8}\n", engine.PreviousKeyMask);
                         text.AppendLine();
-                        // text.AppendFormat("- Chord stagger timer: {0}\n", engine.ChordStaggerTimer);
-                        // text.AppendFormat("- Fat finger timer: {0}\n", engine.FatFingerTimer);
+                        text.AppendFormat("- Chord stagger timer: {0}\n", engine.GetChordStaggerTimer());
+                        text.AppendFormat("- Fat finger timer: {0}\n", engine.GetFatFingerTimer());
+
+                        // Don't strip final newline here, for spacing with the toggle below
+                        GUILayout.Label(text.ToString());
+                        text.Clear();
+
+                        _debugProKeysPressTimesToggle = GUILayout.Toggle(_debugProKeysPressTimesToggle, "Key press times:");
+                        if (_debugProKeysPressTimesToggle)
+                        {
+                            var pressTimes = engine.GetKeyPressTimes();
+                            for (int i = 0; i < pressTimes.Length; i++)
+                            {
+                                text.AppendFormat("- {0}: {1:0.000000}\n", i + 1, pressTimes[i]);
+                            }
+
+                            GUILayout.Label(text.AsSpan().TrimEnd('\n').ToString());
+                            text.Clear();
+                        }
 
                         var stats = proKeysPlayer.Engine.EngineStats;
                         text.AppendLine("\nStats:");
