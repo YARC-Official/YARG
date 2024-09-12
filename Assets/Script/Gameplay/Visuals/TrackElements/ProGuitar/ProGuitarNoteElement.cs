53c1eb1a710ca36781345a8737ed3f7cb47467e5
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-09-12 17:48:32 -0400

    Basic chord shapes for protar

--- Assets/Script/Gameplay/Visuals/TrackElements/ProGuitar/ProGuitarNoteElement.cs
+++ Assets/Script/Gameplay/Visuals/TrackElements/ProGuitar/ProGuitarNoteElement.cs
@@ -2,29 +2,101 @@
 using TMPro;
 using UnityEngine;
 using YARG.Core.Chart;
+using YARG.Core.Engine.ProGuitar;
+using YARG.Core.Logging;
 using YARG.Gameplay.Player;
 
 namespace YARG.Gameplay.Visuals
 {
     public sealed class ProGuitarNoteElement : TrackElement<ProGuitarPlayer>
     {
+        public const int HEIGHT_DEFAULT = 2;
+        public const int HEIGHT_VARIATIONS = 6;
+
         public ProGuitarNote ChordRef { get; set; }
 
         public override double ElementTime => ChordRef.Time;
 
         [SerializeField]
         private TextMeshPro[] _textObjects;
+
         [SerializeField]
-        private GameObject _chordMeshParent;
+        private GameObject _fretObjectsParent;
+        [SerializeField]
+        private ProGuitarSingleFret[] _fretObjects;
+
+        private void Start()
+        {
+            YargLogger.Assert(_textObjects.Length == 6);
+            YargLogger.Assert(_fretObjects.Length == 6);
+        }
 
         protected override void InitializeElement()
         {
-            _chordMeshParent.SetActive(true);
+            var mask = ChordRef.ChordMask;
 
-            foreach (var note in ChordRef.AllNotes)
+            // Find the lowest and highest values
+            byte? min = null;
+            byte? max = null;
+            for (int i = 1; i < 6; i++)
             {
-                _textObjects[note.String].gameObject.SetActive(true);
-                _textObjects[note.String].text = ZString.Format("{0}", note.Fret);
+                var fret = mask[i];
+                if (fret == FretBytes.IGNORE_BYTE)
+                {
+                    continue;
+                }
+
+                if (max is null || fret > max)
+                {
+                    max = fret;
+                }
+
+                if (min is null || fret < min)
+                {
+                    min = fret;
+                }
+            }
+
+            // If a maximum can't be found, that means there are no notes in the chord!
+            if (max is null)
+            {
+                YargLogger.LogWarning("The chord has no notes!");
+                return;
+            }
+
+            _fretObjectsParent.SetActive(true);
+
+            // Display each part of the chord
+            for (int i = 0; i < 6; i++)
+            {
+                var fret = mask[i];
+
+                // If the fret is an ignore byte, it means no note is charted in that position
+                if (fret == FretBytes.IGNORE_BYTE)
+                {
+                    _textObjects[i].gameObject.SetActive(false);
+                    _fretObjects[i].gameObject.SetActive(false);
+                    continue;
+                }
+
+                _textObjects[i].gameObject.SetActive(true);
+                _fretObjects[i].gameObject.SetActive(true);
+
+                _textObjects[i].text = ZString.Format("{0}", fret);
+
+                // Find the height of the fret in the chord
+                int height;
+                if (max == min)
+                {
+                    height = HEIGHT_DEFAULT;
+                }
+                else
+                {
+                    // Find the percent of the height relative to the min and max fret values of the chord
+                    float percent = (float) (fret - min.Value) / (max.Value - min.Value);
+                    height = Mathf.RoundToInt((HEIGHT_VARIATIONS - 1) * percent);
+                }
+                _fretObjects[i].Initialize(height);
             }
         }
 
@@ -34,7 +106,7 @@ namespace YARG.Gameplay.Visuals
 
         protected override void HideElement()
         {
-            _chordMeshParent.SetActive(false);
+            _fretObjectsParent.SetActive(false);
 
             foreach (var text in _textObjects)
             {
---
+++ Assets/Script/Gameplay/Visuals/TrackElements/ProGuitar/ProGuitarSingleFret.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using YARG.Core.Logging;
+
+namespace YARG.Gameplay.Visuals
+{
+    public class ProGuitarSingleFret : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _heights;
+
+        private void Awake()
+        {
+            YargLogger.Assert(_heights.Length == ProGuitarNoteElement.HEIGHT_VARIATIONS);
+        }
+
+        public void Initialize(int height)
+        {
+            foreach (var h in _heights)
+            {
+                h.SetActive(false);
+            }
+
+            _heights[height].SetActive(true);
+        }
+    }
+}
