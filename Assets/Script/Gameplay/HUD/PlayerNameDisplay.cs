1511089da74f1bddd5075cfb50bd4774bf869647
Thome Valentin <thomeval@hotmail.com>
2024-11-29 10:25:52 +0200

    Changed font for player name in vocals HUD to match non-vocals players. Changed harmony icons to be all white, and assign colour at run time based on Harmony index. Moved instrument icon to be behind countdown (looks much better if the countdown displays at the beginning of a song) Updated branch from latest changes in dev branch.

--- Assets/Script/Gameplay/HUD/PlayerNameDisplay.cs
+++ Assets/Script/Gameplay/HUD/PlayerNameDisplay.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
 using YARG.Core;
+using YARG.Core.Logging;
+using YARG.Gameplay.Player;
 using YARG.Helpers.Extensions;
 using YARG.Player;
 
@@ -47,7 +50,32 @@ namespace YARG.Gameplay.HUD
         {
             var materialPath = $"VocalNeedle/{needleId}";
             _needleIcon.material = Addressables.LoadAssetAsync<Material>(materialPath).WaitForCompletion();
+            _instrumentIcon.color = GetHarmonyColor(player);
             ShowPlayer(player);
+        }
+
+        // TODO: Temporary until color profiles for vocals. Duplicated from VocalTrack class.
+        public readonly Color[] HarmonyColors =
+        {
+            new(0f, 0.800f, 1f, 1f),
+            new(1f, 0.522f, 0f, 1f),
+            new(1f, 0.859f, 0f, 1f)
+        };
+
+        private Color GetHarmonyColor(YargPlayer player)
+        {
+            if (player.Profile.CurrentInstrument != Instrument.Harmony)
+            {
+                return Color.white;
+            }
+
+            if (player.Profile.HarmonyIndex >= HarmonyColors.Length)
+            {
+                YargLogger.LogWarning("PlayerNameDisplay", $"Harmony index {player.Profile.HarmonyIndex} is out of bounds.");
+                return Color.white;
+            }
+
+            return HarmonyColors[player.Profile.HarmonyIndex];
         }
 
         private IEnumerator AnimationCoroutine()
