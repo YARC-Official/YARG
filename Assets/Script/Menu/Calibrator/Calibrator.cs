87b55e4723ba0d2740f5fc7667eb386f6185149e
Edwin Dornbierer <dornbier@newacp.ch>
2025-02-04 11:23:26 +0100

    Add video calibration

--- Assets/Script/Menu/Calibrator/Calibrator.cs
+++ Assets/Script/Menu/Calibrator/Calibrator.cs
@@ -24,6 +24,9 @@ namespace YARG.Menu.Calibrator
         private enum State
         {
             Starting,
+            VideoWaiting,
+            Video,
+            VideoDone,
             AudioWaiting,
             Audio,
             AudioDone
@@ -33,11 +36,14 @@ namespace YARG.Menu.Calibrator
         private GameObject _startingStateContainer;
 
         [SerializeField]
-        private GameObject _audioCalibrateContainer;
+        private GameObject _calibrateContainer;
 
         [Space]
         [SerializeField]
-        private TextMeshProUGUI _audioCalibrateText;
+        private TextMeshProUGUI _infoText;
+
+        [SerializeField]
+        private TextMeshProUGUI _detectedText;
 
         private State _state = State.Starting;
         private readonly List<double> _calibrationTimes = new();
@@ -47,6 +53,7 @@ namespace YARG.Menu.Calibrator
         private StemMixer? _mixer;
         private double _time;
 #nullable disable
+        private uint _timerCount = 0;
 
         private void Start()
         {
@@ -78,17 +85,23 @@ namespace YARG.Menu.Calibrator
 
             switch (_state)
             {
+                case State.VideoWaiting:
+                    _state = State.Video;
+                    UpdateForState();
+                    break;
                 case State.AudioWaiting:
                     _state = State.Audio;
                     UpdateForState();
                     break;
+                case State.Video:
                 case State.Audio:
-                    _audioCalibrateText.color = Color.green;
-                    _audioCalibrateText.text = Localize.Key("Menu.Calibrator.Detected");
-
                     _calibrationTimes.Add(Time.realtimeSinceStartupAsDouble - _time);
+
+                    _detectedText.color = Color.green;
+                    _detectedText.text = Localize.Key("Menu.Calibrator.Detected");
                     break;
                 case State.Starting:
+                case State.VideoDone:
                 case State.AudioDone:
                     if (input.GetAction<MenuAction>() == MenuAction.Red)
                     {
@@ -100,13 +113,28 @@ namespace YARG.Menu.Calibrator
 
         private void Update()
         {
+            var color = Color.white;
             switch (_state)
             {
+                case State.Video:
+                    // Fade out text
+                    color = _infoText.color;
+                    color.a -= Time.deltaTime * 9f;
+                    _infoText.color = color;
+
+                    color = _detectedText.color;
+                    color.a -= Time.deltaTime * 3f;
+                    _detectedText.color = color;
+                    break;
                 case State.Audio:
                     // Fade out text
-                    var color = _audioCalibrateText.color;
+                    color = _infoText.color;
                     color.a -= Time.deltaTime * 3f;
-                    _audioCalibrateText.color = color;
+                    _infoText.color = color;
+
+                    color = _detectedText.color;
+                    color.a -= Time.deltaTime * 3f;
+                    _detectedText.color = color;
                     break;
             }
         }
@@ -117,26 +145,66 @@ namespace YARG.Menu.Calibrator
             _mixer = null;
 
             StopAllCoroutines();
+            CancelInvoke();
 
             _startingStateContainer.SetActive(false);
-            _audioCalibrateContainer.SetActive(false);
+            _calibrateContainer.SetActive(false);
+
+            _infoText.fontSize = 64;
+
+            int calibration = -10000;
 
             switch (_state)
             {
                 case State.Starting:
                     _startingStateContainer.SetActive(true);
                     break;
-                case State.AudioWaiting:
-                    _audioCalibrateContainer.SetActive(true);
+
+                case State.VideoWaiting:
+                    _calibrateContainer.SetActive(true);
                     _player = null;
 
-                    _audioCalibrateText.color = Color.white;
-                    _audioCalibrateText.text =
+                    _infoText.color = Color.white;
+                    _infoText.text =
+                        "Press any button on each circle you see.\n" +
+                        "Press any button when you are ready.";
+                    break;
+                case State.Video:
+                    _calibrateContainer.SetActive(true);
+                    _calibrationTimes.Clear();
+
+                    _timerCount = 0;
+                    _time = Time.realtimeSinceStartupAsDouble;
+                    InvokeRepeating("VideoCalibrationTimer", SECONDS_PER_BEAT, SECONDS_PER_BEAT);
+                    break;
+                case State.VideoDone:
+                    _calibrateContainer.SetActive(true);
+                    _detectedText.text = "";
+
+                    calibration = CalculateLatency();
+                    if (calibration > -10000)
+                    {
+                        SettingsManager.Settings.VideoCalibration.Value = calibration;
+
+                        // Set text
+                        _infoText.color = Color.green;
+                        _infoText.text =
+                            $"Calibration set to {calibration}ms!\n" +
+                            "Press back to exit.";
+                    }
+                    break;
+
+                case State.AudioWaiting:
+                    _calibrateContainer.SetActive(true);
+                    _player = null;
+
+                    _infoText.color = Color.white;
+                    _infoText.text =
                         "Press any button on each tick you hear.\n" +
                         "Press any button when you are ready.";
                     break;
                 case State.Audio:
-                    _audioCalibrateContainer.SetActive(true);
+                    _calibrateContainer.SetActive(true);
                     _calibrationTimes.Clear();
 
                     const float SPEED = 1f;
@@ -149,13 +217,27 @@ namespace YARG.Menu.Calibrator
                     StartCoroutine(AudioCalibrateCoroutine());
                     break;
                 case State.AudioDone:
-                    _audioCalibrateContainer.SetActive(true);
-                    CalculateAudioLatency();
+                    _calibrateContainer.SetActive(true);
+                    _detectedText.text = "";
+
+                    calibration = CalculateLatency();
+                    if (calibration > -10000)
+                    {
+                        if (SettingsManager.Settings.AccountForHardwareLatency.Value)
+                            calibration -= GlobalAudioHandler.PlaybackLatency;
+                        SettingsManager.Settings.AudioCalibration.Value = calibration;
+
+                        // Set text
+                        _infoText.color = Color.green;
+                        _infoText.text =
+                            $"Calibration set to {calibration}ms!\n" +
+                            "Press back to exit.";
+                    }
                     break;
             }
         }
 
-        private void CalculateAudioLatency()
+        private int CalculateLatency()
         {
             // Drop all discrepancies
             for (int i = _calibrationTimes.Count - 1; i > 1; i--)
@@ -169,9 +251,9 @@ namespace YARG.Menu.Calibrator
             // If there isn't enough data, RIP
             if (_calibrationTimes.Count <= 8)
             {
-                _audioCalibrateText.color = Color.red;
-                _audioCalibrateText.text = Localize.Key("Menu.Calibrator.NotEnoughData");
-                return;
+                _infoText.color = Color.red;
+                _infoText.text = Localize.Key("Menu.Calibrator.NotEnoughData");
+                return -10000;
             }
 
             // Get the deviations
@@ -217,35 +299,63 @@ namespace YARG.Menu.Calibrator
             int mid = diffs.Count / 2;
             double median = diffs.Count % 2 != 0 ? diffs[mid] : (diffs[mid] + diffs[mid - 1]) / 2f;
 
-            // Set calibration
-            int calibration = (int)Math.Round(median * 1000);
-            if (SettingsManager.Settings.AccountForHardwareLatency.Value)
-                calibration -= GlobalAudioHandler.PlaybackLatency;
-            SettingsManager.Settings.AudioCalibration.Value = calibration;
+            // return median
+            return (int)Math.Round(median * 1000);
+        }
 
-            // Set text
-            _audioCalibrateText.color = Color.green;
-            _audioCalibrateText.text =
-                $"Calibration set to {calibration}ms!\n" +
-                "Press back to exit.";
+        private void VideoCalibrationTimer()
+        {
+            _infoText.fontSize = 64;
+            _infoText.color = Color.white;
+            switch (_timerCount)
+            {
+                case 0:
+                    _infoText.text = "4";
+                    break;
+                case 1:
+                    _infoText.text = "3";
+                    break;
+                case 2:
+                    _infoText.text = "2";
+                    break;
+                case 3:
+                    _infoText.text = "1";
+                    break;
+                case < 20:
+                    _infoText.fontSize = 256;
+                    // black circle
+                    _infoText.text = "\u25CF";
+                    break;
+                default:
+                    _state = State.VideoDone;
+                    UpdateForState();
+                    break;
+            }
+            ++_timerCount;
         }
 
         private IEnumerator AudioCalibrateCoroutine()
         {
-            _audioCalibrateText.color = Color.white;
-            _audioCalibrateText.text = "1";
+            _infoText.color = Color.white;
+            _infoText.text = "4";
 
             yield return new WaitUntil(() => _mixer.GetPosition() >= SECONDS_PER_BEAT * 1f);
-            _audioCalibrateText.color = Color.white;
-            _audioCalibrateText.text = "2";
+            _infoText.color = Color.white;
+            _infoText.text = "3";
 
             yield return new WaitUntil(() => _mixer.GetPosition() >= SECONDS_PER_BEAT * 2f);
-            _audioCalibrateText.color = Color.white;
-            _audioCalibrateText.text = "3";
+            _infoText.color = Color.white;
+            _infoText.text = "2";
 
             yield return new WaitUntil(() => _mixer.GetPosition() >= SECONDS_PER_BEAT * 3f);
-            _audioCalibrateText.color = Color.white;
-            _audioCalibrateText.text = "4";
+            _infoText.color = Color.white;
+            _infoText.text = "1";
+        }
+
+        public void StartVideoMode()
+        {
+            _state = State.VideoWaiting;
+            UpdateForState();
         }
 
         public void StartAudioMode()
