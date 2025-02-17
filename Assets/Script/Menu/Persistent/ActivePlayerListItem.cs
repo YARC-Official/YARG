62d957a0e7e701351125d206128c306b964e2a25
thomeval <thomeval@hotmail.com>
2025-02-17 19:59:08 +0000

    Show warning when player has no input device or microphone (#962)
    * Display a warning on the Difficulty Select screen when a human vocalist has no microphone connected
    Added a "no microphone" icon, and use it in the Active Players display.
    
    * Implement "No Input Device" warning message for non-vocalists
    
    * Add basic No Instrument icons for all instruments
    
    * Remove one unused item from AssetGroup
    
    * Update Instrument Icons (and No Instrument Icons)
    
    * Adjust alignment of warning message (fixes issue with non 16:9 aspect ratios)

--- Assets/Script/Menu/Persistent/ActivePlayerListItem.cs
+++ Assets/Script/Menu/Persistent/ActivePlayerListItem.cs
@@ -3,8 +3,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
-using YARG.Core.Game;
 using YARG.Helpers.Extensions;
+using YARG.Player;
 
 namespace YARG.Menu.Persistent
 {
@@ -21,16 +21,27 @@ namespace YARG.Menu.Persistent
             set => _playerNameText.gameObject.SetActive(value);
         }
 
-        public void Initialize(YargProfile profile)
+        public void Initialize(YargPlayer player)
         {
-            if (profile == null)
+            if (player == null)
             {
-                throw new ArgumentNullException(nameof(profile));
+                throw new ArgumentNullException(nameof(player));
             }
 
-            _playerNameText.text = profile.Name;
+            var profile = player.Profile;
+            if (profile == null)
+            {
+                throw new ArgumentNullException(nameof(player.Profile));
+            }
+
+           _playerNameText.text = profile.Name;
+
+            var instrumentSprite =player.IsMissingInputDevice || player.IsMissingMicrophone
+                 ? $"NoInstrumentIcons[{profile.CurrentInstrument.ToResourceName()}]"
+                 : $"InstrumentIcons[{profile.CurrentInstrument.ToResourceName()}]";
+
             _playerInstrumentIcon.sprite = Addressables
-                .LoadAssetAsync<Sprite>($"InstrumentIcons[{profile.CurrentInstrument.ToResourceName()}]")
+                .LoadAssetAsync<Sprite>(instrumentSprite)
                 .WaitForCompletion();
         }
     }
