bfb9a99eddc6df7d6b1dc0766107fca07283569e
wyrdough <nathan@nwacg.net>
2025-04-17 22:54:58 -0400

    Add "breathing" on beatline to sunburst effect.

--- Assets/Script/Gameplay/Visuals/SunburstEffects.cs
+++ Assets/Script/Gameplay/Visuals/SunburstEffects.cs
@@ -1,9 +1,11 @@
+using DG.Tweening;
 using UnityEngine;
+using YARG.Core.Chart;
 using YARG.Settings;
 
 namespace YARG.Gameplay.Visuals
 {
-    public class SunburstEffects : MonoBehaviour
+    public class SunburstEffects : GameplayBehaviour
     {
         [SerializeField]
         private GameObject _grooveSunburstEffect;
@@ -16,6 +18,16 @@ namespace YARG.Gameplay.Visuals
         [SerializeField]
         private GameObject _starpowerLightEffect;
 
+        private Tweener _grooveTween;
+        private Tweener _starpowerTween;
+        private Vector3 _originalScale;
+
+        protected override void GameplayAwake()
+        {
+            GameManager.BeatEventHandler.Subscribe(PulseSunburst);
+            _originalScale = _grooveSunburstEffect.transform.localScale;
+        }
+
         public void SetSunburstEffects(bool groove, bool starpower)
         {
             starpower &= SettingsManager.Settings.StarPowerHighwayFx.Value != StarPowerHighwayFxMode.Off;
@@ -25,12 +37,42 @@ namespace YARG.Gameplay.Visuals
 
             _starpowerSunburstEffect.SetActive(starpower);
             _starpowerLightEffect.SetActive(starpower);
+
+            _grooveTween ??= _grooveSunburstEffect.transform.DOScale(_originalScale * 0.85f, 0.25f).SetAutoKill(false)
+                .SetEase(Ease.OutSine).Pause();
+
+            _starpowerTween ??= _starpowerSunburstEffect.transform.DOScale(_originalScale * 0.85f, 0.25f).SetAutoKill(false)
+                .SetEase(Ease.InSine).Pause();
         }
 
         private void Update()
         {
             _grooveSunburstEffect.transform.Rotate(0f, 0f, Time.deltaTime * -25f);
             _starpowerSunburstEffect.transform.Rotate(0f, 0f, Time.deltaTime * -25f);
+        }
+
+        private void PulseSunburst(Beatline beatline)
+        {
+            if (_grooveSunburstEffect.activeInHierarchy)
+            {
+                // Snap to full size and tween to smaller size
+                _grooveSunburstEffect.transform.localScale = _originalScale;
+                _grooveTween?.Restart();
+            }
+
+            if (_starpowerSunburstEffect.activeInHierarchy)
+            {
+                // Snap to full size and tween to smaller size
+                _starpowerSunburstEffect.transform.localScale = _originalScale;
+                _starpowerTween?.Restart();
+            }
+        }
+
+        protected override void GameplayDestroy()
+        {
+            _grooveTween?.Kill();
+            _starpowerTween?.Kill();
+            GameManager.BeatEventHandler.Unsubscribe(PulseSunburst);
         }
     }
 }
