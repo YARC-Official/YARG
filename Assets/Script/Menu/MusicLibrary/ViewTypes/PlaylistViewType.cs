14140f2667c939fd9251538f5d4fbd620218fd1b
wyrdough <nathan@nwacg.net>
2025-05-28 05:57:54 -0400

    Allow users to create and curate custom playlists.

--- Assets/Script/Menu/MusicLibrary/ViewTypes/PlaylistViewType.cs
+++ Assets/Script/Menu/MusicLibrary/ViewTypes/PlaylistViewType.cs
@@ -10,7 +10,6 @@ namespace YARG.Menu.MusicLibrary
 {
     public class PlaylistViewType : CategoryViewType
     {
-        public override         BackgroundType          Background => BackgroundType.Normal;
         private static readonly Dictionary<int, Sprite> Sprites     = new();
         private const           string                  ICON_PATH   = "MusicLibraryIcons[Playlists]";
         private const           int                     PLAYLIST_ID = 1;
@@ -19,20 +18,35 @@ namespace YARG.Menu.MusicLibrary
         private readonly        Sprite                  _sprite;
 
         public PlaylistViewType(string primary, int songCount, SongEntry[] songsUnderCategory, Playlist playlist,
-            Action clickAction = null, int id = -1) : base(primary, songCount, songsUnderCategory, clickAction)
+            Action clickAction = null) : base(primary, songCount, songsUnderCategory, clickAction)
         {
             Playlist = playlist;
-            ID = id;
+
+            if (!Sprites.TryGetValue(PLAYLIST_ID, out _sprite))
+            {
+                Sprites.Add(PLAYLIST_ID, _sprite = Addressables.LoadAssetAsync<Sprite>(ICON_PATH).WaitForCompletion());
+            }
         }
 
         // I do not like that GetSongsFromPlaylist has to do the work twice, but songs can't be cached since
         // it has to be static and I'm not sufficiently familiar with C# to figure out how to make CategoryViewType
         // use an initializer we can override.
-        public PlaylistViewType(string primary, Playlist playlist, Action clickAction = null, int id = -1) :
+        public PlaylistViewType(string primary, Playlist playlist, Action clickAction = null) :
             base(primary, GetSongsFromPlaylist(playlist).Length, GetSongsFromPlaylist(playlist), clickAction)
         {
             Playlist = playlist;
-            ID = id;
+
+            if (!Sprites.TryGetValue(PLAYLIST_ID, out _sprite))
+            {
+                Sprites.Add(PLAYLIST_ID, _sprite = Addressables.LoadAssetAsync<Sprite>(ICON_PATH).WaitForCompletion());
+            }
+        }
+
+#nullable enable
+        public override Sprite? GetIcon()
+#nullable disable
+        {
+            return _sprite;
         }
 
         public static SongEntry[] GetSongsFromPlaylist(Playlist playlist)
