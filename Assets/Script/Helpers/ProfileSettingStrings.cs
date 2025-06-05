81b48404f09c5175d0d5e9d0a865b4d79b615236
Will Frick <willfrickvt@gmail.com>
2025-06-05 00:10:43 -0400

    Change static profile setting strings to consts

--- Assets/Script/Helpers/ProfileSettingStrings.cs
+++ Assets/Script/Helpers/ProfileSettingStrings.cs
@@ -9,21 +9,21 @@ namespace YARG.Assets.Script.Helpers
     internal static class ProfileSettingStrings
     {
         // Universal
-        public static string INSTRUMENT_SELECT = "Instrument Select";
-        public static string ENGINE_PRESET = "Engine Preset";
-        public static string THEME_SELECT = "Theme Select";
-        public static string COLOR_PROFILE_SELECT = "Color Profile Select";
-        public static string CAMERA_PRESET = "Camera Preset";
-        public static string HIGHWAY_PRESET = "Highway Preset";
-        public static string INPUT_CALIBRATION = "Input Calibration";
-        public static string NOTE_SPEED_AND_HIGHWAY_LENGTH = "Note Speed and Highway Length";
+        public const string INSTRUMENT_SELECT = "Instrument Select";
+        public const string ENGINE_PRESET = "Engine Preset";
+        public const string THEME_SELECT = "Theme Select";
+        public const string COLOR_PROFILE_SELECT = "Color Profile Select";
+        public const string CAMERA_PRESET = "Camera Preset";
+        public const string HIGHWAY_PRESET = "Highway Preset";
+        public const string INPUT_CALIBRATION = "Input Calibration";
+        public const string NOTE_SPEED_AND_HIGHWAY_LENGTH = "Note Speed and Highway Length";
 
         // GameMode-specific
-        public static string LEFTY_FLIP = "Lefty Flip";
-        public static string RANGE_DISABLE = "Range Disable";
-        public static string SPLIT_TOM_AND_CYMBAL_LANES_IN_PRO_DRUMS = "Split Tom and Cymbal Lanes in Pro Drums";
-        public static string USE_CYMBAL_MODELS = "Use Cymbal Models";
-        public static string SWAP_SNARE_AND_HI_HAT = "Swap Snare and Hi-Hat";
-        public static string SWAP_CRASH_AND_RIDE = "Swap Crash and Ride";
+        public const string LEFTY_FLIP = "Lefty Flip";
+        public const string RANGE_DISABLE = "Range Disable";
+        public const string SPLIT_TOM_AND_CYMBAL_LANES_IN_PRO_DRUMS = "Split Tom and Cymbal Lanes in Pro Drums";
+        public const string USE_CYMBAL_MODELS = "Use Cymbal Models";
+        public const string SWAP_SNARE_AND_HI_HAT = "Swap Snare and Hi-Hat";
+        public const string SWAP_CRASH_AND_RIDE = "Swap Crash and Ride";
     }
 }
