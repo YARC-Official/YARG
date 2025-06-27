e0a5345c762455780aa3578a741cb9f4caadf40f
wyrdough <nathan@nwacg.net>
2025-06-26 21:56:26 -0400

    add venue character component and manager

---
+++ Assets/Script/Venue/Characters/CharacterManager.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using YARG.Core;
+using YARG.Core.Chart;
+using YARG.Core.IO;
+using YARG.Gameplay;
+
+namespace YARG.Venue.Characters
+{
+    public class CharacterManager : GameplayBehaviour
+    {
+
+        [SerializeField]
+        private GameObject _venue;
+
+        private Dictionary<VenueCharacter.CharacterType, VenueCharacter> _characters = new();
+
+        // Ugh, the different note types ruin me again
+        private List<VocalsPhrase> _vocalNotes;
+        private List<DrumNote> _drumNotes;
+        private List<GuitarNote> _guitarNotes;
+        private List<GuitarNote> _bassNotes;
+        private List<GuitarNote> _keysNotes;
+        private List<ProKeysNote> _proKeysNotes;
+
+        private int _guitarNoteIndex;
+        private int _bassNoteIndex;
+        private int _keysNoteIndex;
+        private int _proKeysNoteIndex;
+        private int _drumNoteIndex;
+        private int _vocalNoteIndex;
+
+        private List<TempoChange> _tempoList;
+        private int               _tempoIndex;
+        private int               _previousTempoIndex;
+        private TempoChange       _currentTempo;
+
+        protected override void OnChartLoaded(SongChart chart)
+        {
+            // Find all the VenueCharacters in the venue
+
+            var _venueCharacters = _venue.GetComponentsInChildren<VenueCharacter>(true);
+
+            foreach (var character in _venueCharacters)
+            {
+                _characters.Add(character.Type, character);
+            }
+
+            // Get the expert notes for each track
+            var guitarId = chart.FiveFretGuitar.GetDifficulty(Difficulty.Expert);
+            var bassId = chart.FiveFretBass.GetDifficulty(Difficulty.Expert);
+            var keysId = chart.Keys.GetDifficulty(Difficulty.Expert);
+            var proKeysId = chart.ProKeys.GetDifficulty(Difficulty.Expert);
+            var vocalsId = chart.Vocals.Parts[0];
+            var drumsId = chart.ProDrums.GetDifficulty(Difficulty.Expert);
+
+            _guitarNotes = guitarId.Notes;
+            _bassNotes = bassId.Notes;
+            _keysNotes = keysId.Notes;
+            _proKeysNotes = proKeysId.Notes;
+            _vocalNotes = vocalsId.NotePhrases;
+            _drumNotes = drumsId.Notes;
+
+            _tempoList = chart.SyncTrack.Tempos;
+
+            // Don't animate until there are notes (at least until we have idle animations)
+            foreach (var character in _characters.Values)
+            {
+                character.StopAnimation();
+            }
+        }
+
+        private void Update()
+        {
+            bool tempoChanged = false;
+
+            while (_tempoList.Count > 0 && _tempoIndex < _tempoList.Count &&
+                _tempoList[_tempoIndex].Time <= GameManager.SongTime)
+            {
+                _currentTempo = _tempoList[_tempoIndex];
+                tempoChanged = true;
+                _tempoIndex++;
+            }
+
+            // For each character, use its type to determine which track it is on, then trigger an animation if the note type is correct for that
+            foreach (var key in _characters.Keys)
+            {
+                var character = _characters[key];
+
+                if (tempoChanged)
+                {
+                    character.UpdateTempo(_currentTempo.SecondsPerBeat);
+                }
+                switch (key)
+                {
+                    case VenueCharacter.CharacterType.Guitar:
+                        ProcessGuitar(character);
+                        break;
+                    case VenueCharacter.CharacterType.Bass:
+                        ProcessBass(character);
+                        break;
+                    case VenueCharacter.CharacterType.Vocals:
+                        ProcessVocals(character);
+                        break;
+                    case VenueCharacter.CharacterType.Drums:
+                        ProcessDrums(character);
+                        break;
+                }
+            }
+        }
+
+        private void ProcessGuitar(VenueCharacter character)
+        {
+            while (_guitarNotes.Count > 0 && _guitarNoteIndex < _guitarNotes.Count && _guitarNotes[_guitarNoteIndex].Time - character.TimeToFirstHit <= GameManager.SongTime + character.TimeToFirstHit)
+            {
+                if (_guitarNoteIndex >= _guitarNotes.Count)
+                {
+                    break;
+                }
+
+                var note = _guitarNotes[_guitarNoteIndex];
+                _guitarNoteIndex++;
+
+                if (!character.IsAnimating())
+                {
+                    character.StartAnimation(_currentTempo.SecondsPerBeat);
+                    return;
+                }
+
+                // If next note is more than secondsPerBeat away, stop animating
+                if (note.NextNote == null || note.NextNote.Time > GameManager.SongTime + (_currentTempo.SecondsPerBeat * 2))
+                {
+                    character.StopAnimation();
+                }
+
+                // Trigger the strum animation
+                // if (note.IsStrum)
+                // {
+                //     character.OnNote(note);
+                // }
+            }
+        }
+
+        private void ProcessBass(VenueCharacter character)
+        {
+            while (_bassNotes.Count > 0 && _bassNoteIndex < _bassNotes.Count && _bassNotes[_bassNoteIndex].Time - character.TimeToFirstHit <= GameManager.SongTime)
+            {
+                if (_bassNoteIndex >= _bassNotes.Count)
+                {
+                    break;
+                }
+
+                var note = _bassNotes[_bassNoteIndex];
+                _bassNoteIndex++;
+
+                if (!character.IsAnimating())
+                {
+                    character.StartAnimation(_currentTempo.SecondsPerBeat);
+                    return;
+                }
+
+                // If next note is more than secondsPerBeat away, stop animating
+                if (note.NextNote == null || note.NextNote.Time > GameManager.SongTime + _currentTempo.SecondsPerBeat * 2)
+                {
+                    character.StopAnimation();
+                }
+
+
+                // Trigger the strum animation
+                // if (note.IsStrum)
+                // {
+                //     character.OnNote(note);
+                // }
+            }
+        }
+
+        private void ProcessVocals(VenueCharacter character)
+        {
+
+        }
+
+        private void ProcessDrums(VenueCharacter character)
+        {
+            while (_drumNotes.Count > 0 && _drumNoteIndex < _drumNotes.Count &&
+                _drumNotes[_drumNoteIndex].Time - character.TimeToFirstHit <= GameManager.SongTime)
+            {
+                if (_drumNoteIndex >= _drumNotes.Count)
+                {
+                    break;
+                }
+                var note = _drumNotes[_drumNoteIndex];
+                _drumNoteIndex++;
+
+                if (!character.IsAnimating())
+                {
+                    character.StartAnimation(_currentTempo.SecondsPerBeat);
+                    return;
+                }
+
+                // If next note is more than secondsPerBeat away, stop animating
+                if (note.NextNote == null || note.NextNote.Time > GameManager.SongTime + _currentTempo.SecondsPerBeat * 2)
+                {
+                    character.StopAnimation();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/Venue/Characters/VenueCharacter.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using YARG.Core.Chart;
+using YARG.Core.Logging;
+
+namespace YARG.Venue.Characters
+{
+    public class VenueCharacter : MonoBehaviour
+    {
+        public enum CharacterType
+        {
+            Bass,
+            Guitar,
+            Drums,
+            Vocals,
+            Keys,
+        }
+
+        public enum AnimationStates
+        {
+            Idle,
+            Playing
+        }
+
+        [SerializeField]
+        private CharacterManager _characterManager;
+
+        [SerializeField]
+        public CharacterType Type;
+
+        [SerializeField]
+        private Dictionary<AnimationStates, string> _animationStates;
+
+        [SerializeField]
+        private int _actionsPerAnimationCycle;
+
+        [Space]
+
+        [SerializeField]
+        private bool _enableAnimationStates;
+        [SerializeField]
+        private string _idleAnimationName;
+        [SerializeField]
+        private string _playingAnimationName;
+        [SerializeField]
+        private int _framesToFirstHit;
+
+        private int _idleAnimationHash;
+        private int _playingAnimationHash;
+
+        private RuntimeAnimatorController _animatorController;
+        private Animator _animator;
+
+        private float _animationLength;
+        private float _animationSpeed;
+        private int   _animationParamHash;
+
+        private bool _isAnimating;
+
+        [NonSerialized]
+        public float TimeToFirstHit = 0.0f;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _animatorController = _animator.runtimeAnimatorController;
+
+            // Start in the idle state
+            // _animator.Play(_animationStates[AnimationStates.Idle]);
+
+            // Figure out how long the animation is
+            var clip = _animatorController.animationClips[0];
+            _animationLength = clip.length;
+            TimeToFirstHit = _framesToFirstHit / clip.frameRate;
+
+            _animationParamHash = Animator.StringToHash("SpeedAdjustment");
+
+            if (_enableAnimationStates) {
+                _idleAnimationHash = Animator.StringToHash(_idleAnimationName);
+                _playingAnimationHash = Animator.StringToHash(_playingAnimationName);
+            }
+        }
+
+        public void OnNote<T>(Note<T> note) where T : Note<T>
+        {
+            if (Type != CharacterType.Guitar && Type != CharacterType.Bass)
+            {
+                return;
+            }
+
+            var nextNote = note.NextNote as GuitarNote;
+
+            double delta = Double.MaxValue;
+            if (nextNote != null && nextNote.IsStrum)
+            {
+                delta = note.NextNote.Time - note.Time;
+            }
+            else
+            {
+                _animator.SetFloat(_animationParamHash, 1);
+            }
+
+            // Do we need to change the speed to fit within the time to the next note?
+            // if (delta < _animationLength)
+            // {
+            double speed;
+                // Divide by 14 for guitar, since that's how many strums the animation has
+                if (Type == CharacterType.Guitar)
+                {
+                    speed = (_animationLength / 14) / delta;
+                }
+                else
+                {
+                    speed = _animationLength / delta;
+                }
+                speed += 0.001;
+                _animator.SetFloat(_animationParamHash, (float) speed);
+                // _animator.speed = (float) speed;
+            // }
+            // else
+            // {
+            //     _animator.speed = 1;
+            // }
+
+            if (note is Note<GuitarNote>)
+            {
+                // _animator.Play(_animationStates[AnimationStates.Playing]);
+                if (Type == CharacterType.Bass)
+                {
+                    // _animator.SetTrigger("BassOneCycle");
+                    _animator.Play("Base Layer.BassOneCycle");
+                    return;
+                }
+            }
+
+            if (note is Note<DrumNote>)
+            {
+
+            }
+
+            if (note is Note<VocalNote>)
+            {
+
+            }
+
+            if (note is Note<ProKeysNote>)
+            {
+
+            }
+        }
+
+        public void StopAnimation()
+        {
+            if (!_enableAnimationStates)
+            {
+                return;
+            }
+
+            // _animationSpeed = 0;
+            // _animator.SetFloat(_animationParamHash, 0);
+            // _animator.Play("Base Layer.Idle");
+            _animator.Play(_idleAnimationHash);
+            YargLogger.LogDebug("Starting Idle animation");
+
+            _isAnimating = false;
+        }
+
+        public void StartAnimation(float secondsPerBeat)
+        {
+            if (!_enableAnimationStates)
+            {
+                return;
+            }
+
+            _isAnimating = true;
+
+            UpdateTempo(secondsPerBeat);
+
+            // _animator.Play("91e81ab3-0a89-48c2-b8ce-a23f28bdf736 Skeleton_Merged_mixamo_com_001,BaseLayer_91e81ab3-0a89-48c2-b8ce-a23f28b");
+            _animator.Play(_playingAnimationHash);
+            YargLogger.LogDebug("Starting Strum animation");
+        }
+
+        public bool IsAnimating()
+        {
+            return _isAnimating;
+        }
+
+        public void UpdateTempo(float secondsPerBeat)
+        {
+            // Adjust the speed of the animation based on the song tempo
+            if (_actionsPerAnimationCycle == 0)
+            {
+                return;
+            }
+
+            // We want one animation cycle per beat (as a multiplier, so if there are 17 actions and 0.4 seconds per beat, we want the animation to complete in 6.8 seconds)
+            // var secondsPerAction = _animationLength / _actionsPerAnimationCycle;
+            // var speed = secondsPerBeat / secondsPerAction;
+            var secondsPerAction = _animationLength / _actionsPerAnimationCycle;
+            // We want secondsPerAction to match secondsPerBeat
+            var speed = secondsPerAction / secondsPerBeat;
+            //
+            // var desiredAnimationLength = secondsPerBeat * _actionsPerAnimationCycle;
+            // var speed = _animationLength / desiredAnimationLength;
+            _animationSpeed = speed;
+            _animator.SetFloat(_animationParamHash, speed);
+
+            YargLogger.LogFormatDebug("Adjusting speed of {0} to {1}", Type, speed);
+        }
+    }
+}
