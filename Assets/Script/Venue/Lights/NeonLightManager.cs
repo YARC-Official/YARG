9df58fa4e43b0243b5fe7bd3a9085559ad9ada64
Mekkabit <123285845+Mekkabit@users.noreply.github.com>
2025-06-28 20:20:57 -0700

    Update NeonLightManager.cs
    Add material fields for all locations, color each field separately, store and overwrite color of non-generic fields; generic left the same.

--- Assets/Script/Venue/Lights/NeonLightManager.cs
+++ Assets/Script/Venue/Lights/NeonLightManager.cs
@@ -12,33 +12,185 @@ namespace YARG.Venue
     {
         private static readonly int _emissionMultiplier = Shader.PropertyToID("_Emission_Multiplier");
         private static readonly int _emissionSecondaryColor = Shader.PropertyToID("_Emission_Secondary_Color");
+        private static readonly int _emissionColor = Shader.PropertyToID("_EmissionColor");
 
         [SerializeField]
         private Material[] _neonMaterials;
+		[SerializeField]
+		private Material[] _neonLeftMaterials;
+		[SerializeField]
+		private Material[] _neonRightMaterials;
+		[SerializeField]
+		private Material[] _neonFrontMaterials;
+		[SerializeField]
+		private Material[] _neonBackMaterials;
+		[SerializeField]
+		private Material[] _neonCenterMaterials;
+		[SerializeField]
+		private Material[] _neonCrowdMaterials;
 
         private LightManager _lightManager;
+		private Color _defaultColorLeft;
+		private Color _defaultColorRight;
+		private Color _defaultColorFront;
+		private Color _defaultColorBack;
+		private Color _defaultColorCenter;
+		private Color _defaultColorCrowd;
 
         private void Awake()
         {
             _lightManager = FindObjectOfType<LightManager>();
+			
+			foreach (var material in _neonLeftMaterials)
+			{
+				_defaultColorLeft = material.GetColor(_emissionColor);
+			}
+			
+			foreach (var material in _neonRightMaterials)
+			{
+				_defaultColorRight = material.GetColor(_emissionColor);
+			}
+			
+			foreach (var material in _neonFrontMaterials)
+			{
+				_defaultColorFront = material.GetColor(_emissionColor);
+			}
+			
+			foreach (var material in _neonBackMaterials)
+			{
+				_defaultColorBack = material.GetColor(_emissionColor);
+			}
+			
+			foreach (var material in _neonCenterMaterials)
+			{
+				_defaultColorCenter = material.GetColor(_emissionColor);
+			}
+			
+			foreach (var material in _neonCrowdMaterials)
+			{
+				_defaultColorCrowd = material.GetColor(_emissionColor);
+			}
+			
         }
 
         private void Update()
         {
-            var lightState = _lightManager.GenericLightState;
+            
 
             // Update all of the materials
             foreach (var material in _neonMaterials)
             {
-                material.SetFloat(_emissionMultiplier, lightState.Intensity);
+                var lightState = _lightManager.GenericLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
 
                 if (lightState.Color == null)
                 {
-                    material.SetColor(_emissionSecondaryColor, Color.white);
+					material.SetColor(_emissionSecondaryColor, Color.white);
                 }
                 else
                 {
-                    material.SetColor(_emissionSecondaryColor, lightState.Color.Value);
+					material.SetColor(_emissionSecondaryColor, lightState.Color.Value);
+                }
+            }
+			
+			
+
+            foreach (var material in _neonLeftMaterials)
+            {
+                var lightState = _lightManager.LeftLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
+
+                if (lightState.Color == null)
+                {
+                    material.SetColor(_emissionColor, _defaultColorLeft);
+                }
+                else
+                {
+					material.SetColor(_emissionColor, lightState.Color.Value);
+                }
+            }
+			
+			
+
+            foreach (var material in _neonRightMaterials)
+            {
+                var lightState = _lightManager.RightLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
+
+                if (lightState.Color == null)
+                {
+                    material.SetColor(_emissionColor, _defaultColorRight);
+                }
+                else
+                {
+					material.SetColor(_emissionColor, lightState.Color.Value);
+                }
+            }
+			
+
+            foreach (var material in _neonFrontMaterials)
+            {
+				var lightState = _lightManager.FrontLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
+
+                if (lightState.Color == null)
+                {
+                    material.SetColor(_emissionColor, _defaultColorFront);
+                }
+                else
+                {
+					material.SetColor(_emissionColor, lightState.Color.Value);
+                }
+            }
+			
+			
+
+            foreach (var material in _neonBackMaterials)
+            {
+                var lightState = _lightManager.BackLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
+
+                if (lightState.Color == null)
+                {
+                    material.SetColor(_emissionColor, _defaultColorBack);
+                }
+                else
+                {
+					material.SetColor(_emissionColor, lightState.Color.Value);
+                }
+            }
+			
+			
+
+            foreach (var material in _neonCenterMaterials)
+            {
+                var lightState = _lightManager.CenterLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
+
+                if (lightState.Color == null)
+                {
+                    material.SetColor(_emissionColor, _defaultColorCenter);
+                }
+                else
+                {
+					material.SetColor(_emissionColor, lightState.Color.Value);
+                }
+			}
+			
+			
+
+            foreach (var material in _neonCrowdMaterials)
+            {
+                var lightState = _lightManager.CrowdLightState;
+				material.SetFloat(_emissionMultiplier, lightState.Intensity);
+
+                if (lightState.Color == null)
+                {
+                    material.SetColor(_emissionColor, _defaultColorCrowd);
+                }
+                else
+                {
+					material.SetColor(_emissionColor, lightState.Color.Value);
                 }
             }
         }
