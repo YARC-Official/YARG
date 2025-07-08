0f9bc972a18a31aef117e64ba21bd5399d6c60f1
Mekkabit <123285845+Mekkabit@users.noreply.github.com>
2025-07-08 14:56:48 -0700

    Fix venue lighting staying black from blackout->gradient cues
    Slightly outside the scope of my PR but, related enough? Blackout isn't intended to leave the lights off after it's over and quite a few charts don't have a flare, stomp, strobe, or silhouette cue immediately following blackout.

--- Assets/Script/Venue/Lights/LightManager.Cues.cs
+++ Assets/Script/Venue/Lights/LightManager.Cues.cs
@@ -7,6 +7,7 @@ namespace YARG.Venue
         private LightState AutoGradient(LightState current, Gradient gradient)
         {
             current.Color = gradient.Evaluate(current.Delta);
+			current.Intensity = 1f;
 
             current.Delta += Time.deltaTime * _gradientLightingSpeed;
             if (current.Delta > 1f)
