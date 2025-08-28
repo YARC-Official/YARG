dd82989091e8a48f2e9a210f1c24e75864bfaad8
Mekkabit <123285845+Mekkabit@users.noreply.github.com>
2025-08-28 15:10:50 -0700

    Fix pp hue shift direction, add spotlights to neon manager (#1111)
    * Fix hue shift direction of contrast green/blue in post processing
    
    * Add spotlights to the neon manager

--- Assets/Script/Venue/Lights/NeonLightManager.cs
+++ Assets/Script/Venue/Lights/NeonLightManager.cs
@@ -22,6 +22,7 @@ namespace YARG.Venue
 		public struct NeonFullColor {
 			public Material Material;
 			public VenueLightLocation Location;
+			public VenueSpotLightLocation SpotLocation;
 			[System.NonSerialized] 
 			public Color InitialColor;
 		}
@@ -62,48 +63,76 @@ namespace YARG.Venue
             {
 				var neon = _neonMaterialsFullColor[i];
 				
-				switch (neon.Location)
+				switch ((neon.Location, neon.SpotLocation))
 				{
-					case VenueLightLocation.Generic:
+					case (VenueLightLocation.Generic, VenueSpotLightLocation.None):
 						var lightState = _lightManager.GenericLightState;
 						neon.Material.SetColor(_emissionColor, lightState.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightState.Intensity);
 					break;
 					
-					case VenueLightLocation.Left:
+					case (VenueLightLocation.Left, VenueSpotLightLocation.None):
 						var lightStateLeft = _lightManager.LeftLightState;
 						neon.Material.SetColor(_emissionColor, lightStateLeft.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightStateLeft.Intensity);
 					break;
 					
-					case VenueLightLocation.Right:
+					case (VenueLightLocation.Right, VenueSpotLightLocation.None):
 						var lightStateRight = _lightManager.RightLightState;
 						neon.Material.SetColor(_emissionColor, lightStateRight.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightStateRight.Intensity);
 					break;
 					
-					case VenueLightLocation.Front:
+					case (VenueLightLocation.Front, VenueSpotLightLocation.None):
 						var lightStateFront = _lightManager.FrontLightState;
 						neon.Material.SetColor(_emissionColor, lightStateFront.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightStateFront.Intensity);
 					break;
 					
-					case VenueLightLocation.Back:
+					case (VenueLightLocation.Back, VenueSpotLightLocation.None):
 						var lightStateBack = _lightManager.BackLightState;
 						neon.Material.SetColor(_emissionColor, lightStateBack.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightStateBack.Intensity);
 					break;
 					
-					case VenueLightLocation.Center:
+					case (VenueLightLocation.Center, VenueSpotLightLocation.None):
 						var lightStateCenter = _lightManager.CenterLightState;
 						neon.Material.SetColor(_emissionColor, lightStateCenter.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightStateCenter.Intensity);
 					break;
 					
-					case VenueLightLocation.Crowd:
+					case (VenueLightLocation.Crowd, VenueSpotLightLocation.None):
 						var lightStateCrowd = _lightManager.CrowdLightState;
 						neon.Material.SetColor(_emissionColor, lightStateCrowd.Color ?? neon.InitialColor);
 						neon.Material.SetFloat(_emissionMultiplier, lightStateCrowd.Intensity);
+					break;
+					
+					case (_, VenueSpotLightLocation.Bass):
+						var BassIntensity = neon.Material.GetFloat(_emissionMultiplier);
+						var lightStateBass = _lightManager.GetSpotlightStateFor(VenueSpotLightLocation.Bass);
+						float Bass = Mathf.Lerp(BassIntensity, lightStateBass ? 1f : 0f, Time.deltaTime * 10f);
+						neon.Material.SetFloat(_emissionMultiplier, Bass);
+					break;
+					
+					case (_, VenueSpotLightLocation.Drums):
+						var DrumsIntensity = neon.Material.GetFloat(_emissionMultiplier);
+						var lightStateDrums = _lightManager.GetSpotlightStateFor(VenueSpotLightLocation.Drums);
+						float Drums = Mathf.Lerp(DrumsIntensity, lightStateDrums ? 1f : 0f, Time.deltaTime * 10f);
+						neon.Material.SetFloat(_emissionMultiplier, Drums);
+					break;
+					
+					case (_, VenueSpotLightLocation.Guitar):
+						var GuitarIntensity = neon.Material.GetFloat(_emissionMultiplier);
+						var lightStateGuitar = _lightManager.GetSpotlightStateFor(VenueSpotLightLocation.Guitar);
+						float Guitar = Mathf.Lerp(GuitarIntensity, lightStateGuitar ? 1f : 0f, Time.deltaTime * 10f);
+						neon.Material.SetFloat(_emissionMultiplier, Guitar);
+					break;
+					
+					case (_, VenueSpotLightLocation.Vocals):
+						var VocalsIntensity = neon.Material.GetFloat(_emissionMultiplier);
+						var lightStateVocals = _lightManager.GetSpotlightStateFor(VenueSpotLightLocation.Vocals);
+						float Vocals = Mathf.Lerp(VocalsIntensity, lightStateVocals ? 1f : 0f, Time.deltaTime * 10f);
+						neon.Material.SetFloat(_emissionMultiplier, Vocals);
 					break;
 					
 					default:
