d97da7b8ff241897e4d67a462e047a5bb60d18df
Lauri Niskanen <ape@ape3000.com>
2026-01-11 16:35:42 +0200

    Show high score card also on the first clear

--- Assets/Script/Gameplay/GameManager.cs
+++ Assets/Script/Gameplay/GameManager.cs
@@ -485,7 +485,7 @@ namespace YARG.Gameplay
             {
                 PlayerScores = _players.Select(player => new PlayerScoreCard
                 {
-                    IsHighScore = player.Score > player.LastHighScore,
+                    IsHighScore = player.LastHighScore == null || player.Score > player.LastHighScore.Value,
                     Player = player.Player,
                     Stats = player.BaseStats
                 }).ToArray(),
