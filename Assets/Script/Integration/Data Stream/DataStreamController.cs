8ea3ffdb2e8089dd9bcf788fbafaf0196dbbe822
The Fat Bastid <tfb@sof.ws>
2026-02-03 17:50:28 -0500

    Update DataStreamController.cs

--- Assets/Script/Integration/Data Stream/DataStreamController.cs
+++ Assets/Script/Integration/Data Stream/DataStreamController.cs
@@ -14,11 +14,13 @@ namespace YARG.Integration
     public class DataStreamController :  MonoSingleton<DataStreamController>
     {
         // Queues for instrument notes to prevent missed notes between timer ticks
-        private static readonly Queue<int> _drumQueue = new();
-        private static readonly Queue<int> _guitarQueue = new();
-        private static readonly Queue<int> _bassQueue = new();
-        private static readonly Queue<int> _keysQueue = new();
-        private static readonly object _queueLock = new();
+        // 1/20 being a blink of an eye. It should never lag more than that, and if so shows a larger problem elsewhere.
+        private static readonly int        _defaultSize =  (int)Math.Ceiling(TARGET_FPS * (1 / 20)) + 1;
+        private static readonly Queue<int> _drumQueue   = new(_defaultSize);
+        private static readonly Queue<int> _guitarQueue = new(_defaultSize);
+        private static readonly Queue<int> _bassQueue   = new(_defaultSize);
+        private static readonly Queue<int> _keysQueue   = new(_defaultSize);
+        private static readonly object     _queueLock   = new();
         [Serializable]
         public struct DataMessage
         {
