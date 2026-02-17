960b1cb498db6ee534217e5bd3a95b066df0d205
polson <302882+polson@users.noreply.github.com>
2026-02-17 15:18:48 -0600

    Implement video calibration

--- Assets/Script/Gameplay/Player/TrackPlayer.cs
+++ Assets/Script/Gameplay/Player/TrackPlayer.cs
@@ -261,6 +261,8 @@ namespace YARG.Gameplay.Player
             GameManager.BeatEventHandler.Audio.Unsubscribe(MetronomeTock);
             GameManager.BeatEventHandler.Visual.Unsubscribe(SunburstEffects.PulseSunburst);
 
+            _autoCalibrator?.Dispose();
+
             base.FinishDestruction();
         }
 
@@ -893,7 +895,7 @@ namespace YARG.Gameplay.Player
 
         protected virtual void OnNoteHit(int index, TNote note)
         {
-            if (IsAutoCalibrating && !Player.Profile.IsBot)
+            if (!Player.Profile.IsBot)
             {
                 _autoCalibrator.RecordAccuracy(note.Time);
             }
--- Assets/Script/Helpers/AutoCalibrator.cs
+++ Assets/Script/Helpers/AutoCalibrator.cs
@@ -6,10 +6,11 @@ using YARG.Settings;
 
 using YARG.Gameplay;
 using YARG.Menu.Persistent;
+using YARG.Settings.Types;
 
 namespace YARG.Helpers
 {
-    public class AutoCalibrator
+    public class AutoCalibrator : IDisposable
     {
         // Number of notes to collect before each adjustment
         private const int SAMPLE_SIZE = 20;
@@ -22,14 +23,62 @@ namespace YARG.Helpers
 
         private int _calibration;
 
+        private bool IsCalibratingAudio => CalibrationMode == CalibrationType.AUDIO;
+        private bool IsCalibratingVideo => CalibrationMode == CalibrationType.VIDEO;
+        private IntSetting AudioCalibrationSetting => SettingsManager.Settings.AudioCalibration;
+        private IntSetting VideoCalibrationSetting => SettingsManager.Settings.VideoCalibration;
+        private ToggleSetting AutoAudioSetting => SettingsManager.Settings.AutoCalibrateAudio;
+        private ToggleSetting AutoVideoSetting => SettingsManager.Settings.AutoCalibrateVideo;
+
+        private enum CalibrationType
+        {
+            DISABLED,
+            AUDIO,
+            VIDEO
+        }
+
+        private CalibrationType CalibrationMode =>
+            AutoAudioSetting.Value   ? CalibrationType.AUDIO
+            : AutoVideoSetting.Value ? CalibrationType.VIDEO
+                                       : CalibrationType.DISABLED;
+
         public AutoCalibrator(GameManager gameManager)
         {
             _gameManager = gameManager;
-            _calibration = SettingsManager.Settings.AudioCalibration.Value;
+            AutoAudioSetting.OnChange += AutoCalibrateModeChanged;
+            AutoVideoSetting.OnChange += AutoCalibrateModeChanged;
+        }
+
+        private void AutoCalibrateModeChanged(bool enabled)
+        {
+            Reset();
+        }
+
+        public void Dispose()
+        {
+            AutoAudioSetting.OnChange -= AutoCalibrateModeChanged;
+            AutoVideoSetting.OnChange -= AutoCalibrateModeChanged;
+        }
+
+        private void Reset()
+        {
+            _accuracyList.Clear();
+            if (IsCalibratingAudio)
+            {
+                _calibration = AudioCalibrationSetting.Value;
+            }
+            else if (IsCalibratingVideo)
+            {
+                _calibration = VideoCalibrationSetting.Value;
+            }
         }
 
         public void RecordAccuracy(double noteTime)
         {
+            if (CalibrationMode == CalibrationType.DISABLED)
+            {
+                return;
+            }
 
             double accuracy = (_gameManager.InputTime - noteTime) * 1000;
             _accuracyList.Add(accuracy);
@@ -59,18 +108,26 @@ namespace YARG.Helpers
         private void ApplyAdjustment(int adjustment)
         {
             _calibration += adjustment;
-            SettingsManager.Settings.AudioCalibration.Value = _calibration;
+            if (CalibrationMode == CalibrationType.AUDIO)
+            {
+                AudioCalibrationSetting.Value = _calibration;
+            } else if (CalibrationMode == CalibrationType.VIDEO)
+            {
+                VideoCalibrationSetting.Value = _calibration;
+            }
             _gameManager.UpdateCalibration();
         }
 
         private void NotifyCalibrationUpdated()
         {
-            ToastManager.ToastMessage($"Calibration updated: {_calibration} ms");
+            var type = IsCalibratingAudio ? "Audio" : "Video";
+            ToastManager.ToastMessage($"{type} calibration updated: {_calibration} ms");
         }
 
         private void NotifyCalibrationStable()
         {
-            ToastManager.ToastSuccess($"Auto calibration stable ({_calibration} ms)");
+            var type = IsCalibratingAudio ? "Audio" : "Video";
+            ToastManager.ToastSuccess($"{type} calibration stable ({_calibration} ms)");
         }
 
         // Removes hits near the edges of the hit window.  We find the "middle 50%" of the data (Q1 to Q3),
