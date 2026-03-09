11dd4b6f19e46b3609ecefcbf5e801de76efbdd0
Mekkabit <123285845+Mekkabit@users.noreply.github.com>
2026-03-09 14:51:12 -0700

    missed a .rgb

--- Assets/Art/Shaders/VenueShaders/VenuePP.hlsl
+++ Assets/Art/Shaders/VenueShaders/VenuePP.hlsl
@@ -132,7 +132,7 @@ half4 YargVenuePP(half3 col, float2 uvDistorted, float2 uv)
 
     if(_YargTrailLength > 0)
     {
-        half3 prevCol = SAMPLE_TEXTURE2D(_YargPrevFrame, sampler_LinearClamp, uv).rgb;
+        half3 prevCol = SAMPLE_TEXTURE2D(_YargPrevFrame, sampler_LinearClamp, uv);
         half luma = dot(prevCol, half3(0.2126, 0.7152, 0.0722));
         half mask = step(0.15, luma);
         col = max(col, prevCol * (mask - _YargTrailLength / 2));
