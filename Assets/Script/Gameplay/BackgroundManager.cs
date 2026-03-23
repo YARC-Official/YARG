c87b5d2570b7a91e91e55ba2ba059019a751d78c
Anton Romanov <theli.ua@gmail.com>
2026-03-22 17:31:23 -0700

    Future proof asset bundle loading by deriving metal shader subbundle name from the name of main bundle

--- Assets/Script/Gameplay/BackgroundManager.cs
+++ Assets/Script/Gameplay/BackgroundManager.cs
@@ -570,6 +570,13 @@ namespace YARG.Gameplay
                 shaderBundleName
             );
 
+            if (shaderBundleData == null)
+            {
+                shaderBundleData = (TextAsset)await bundle.LoadAssetAsync<TextAsset>(
+                    bundle.name + BundleBackgroundManager.BUNDLE_OSX_SUFFIX
+                );
+            }
+
             if (shaderBundleData != null && shaderBundleData.bytes.Length > 0)
             {
                 YargLogger.LogInfo("Loading Metal shader bundle");
