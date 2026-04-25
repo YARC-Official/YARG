8cde1be9034da8d22b7ee18f323d47de27606fb6
wyrdough <nathan@nwacg.net>
2026-04-25 04:49:21 -0400

    Get lighting closer to original logic

--- Assets/Script/Venue/Animator/Channels/LightingChannel.cs
+++ Assets/Script/Venue/Animator/Channels/LightingChannel.cs
@@ -36,20 +36,34 @@ namespace YARG.Venue
                     ? _hashes.LightDefault
                     : _hashes.LightingBlendHashes[(int) e.Type];
 
-                // Crossfade if same event repeats and there is a known next event
-                if (i + 1 < events.Count)
+                int nexti = i + 1;
+
+                while (nexti < events.Count && (events[nexti].Type is LightingType.KeyframeFirst
+                    or LightingType.KeyframeNext or LightingType.KeyframePrevious))
                 {
-                    var next = events[i + 1];
+                    nexti++;
+                }
+
+                // Crossfade if same event repeats and there is a known next event
+                if (nexti < events.Count && events[i].Type == events[nexti].Type && e.Type
+                    is not LightingType.KeyframeFirst and not LightingType.KeyframeNext
+                    and not LightingType.KeyframePrevious)
+                {
+                    var next = events[nexti];
                     int nextHash = next.Type is LightingType.Default or LightingType.Intro
                         ? _hashes.LightDefault
                         : _hashes.LightingBlendHashes[(int) next.Type];
 
-                    if (hash == nextHash)
-                    {
-                        float duration = (float) (next.Time - e.Time);
-                        queue.Add(AnimatorCommand.Blend(t, _animator, nextHash, duration, _lightingLayerHash));
-                        continue;
-                    }
+                    float duration = (float) (next.Time - e.Time);
+                    queue.Add(AnimatorCommand.Blend(t, _animator, nextHash, duration, _lightingLayerHash));
+                    continue;
+                }
+
+                // Last event of certain types is handled differently
+                if (i == events.Count - 1 && e.Type is LightingType.Frenzy
+                    or LightingType.CoolAutomatic or LightingType.WarmAutomatic)
+                {
+                    queue.Add(AnimatorCommand.Float(t, _animator, _hashes.BPMAdjust, 0f));
                 }
 
                 queue.Add(AnimatorCommand.Trigger(t, _animator, hash));
