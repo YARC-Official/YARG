6b747e3b75d21e6d6e345b6671c749a41d55adcb
Supersonicboss1 <leodalgado@gmail.com>
2026-06-26 15:51:59 +0100

    Add localization support to the pause menu

---
+++ Assets/Script/Gameplay/HUD/Pause/GenericPauseOption.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using YARG.Localization;
+
+namespace YARG.Gameplay.HUD
+{
+    public class GenericPauseOption : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _optionText;
+
+        [SerializeField]
+        private string _localizationKey;
+
+        public void Awake()
+        {
+            if (_localizationKey == string.Empty)
+            {
+                return;
+            }
+            _optionText.text = Localize.Key(_localizationKey);
+        }
+    }
+}
