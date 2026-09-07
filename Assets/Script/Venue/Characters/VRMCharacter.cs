f78a1fad9dc889e1c6c90ff5eb579b09d00999ce
dispel <101132940+4dispel@users.noreply.github.com>
2026-09-07 22:26:56 +0200

    make both venue rngs even 1 to 100 ranges (#1668)
    * make both venue RNG values even 1 to 100 ranges
    
    * swap start and end index because that is significant now

--- Assets/Script/Venue/Characters/VRMCharacter.cs
+++ Assets/Script/Venue/Characters/VRMCharacter.cs
@@ -136,7 +136,8 @@ namespace YARG.Venue.Characters
 
             if (HasRng)
             {
-                var random = UnityEngine.Random.Range(0, 9);
+                //end is max exclusive intended range is 1-100
+                var random = UnityEngine.Random.Range(1, 101);
                 _animator.SetInteger(_rngHash, random);
                 CurrentRng = random;
             }
