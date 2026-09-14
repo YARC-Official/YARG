4de6c3794d54ffc3508784803732d8b39bdbc063
Will Frick <willfrickvt@gmail.com>
2026-09-13 21:26:05 -0400

    Gameplay (button) bindings wire up correctly!

--- Assets/Script/Input/Bindings/Runtime/RuntimeSingleButtonBinding.cs
+++ Assets/Script/Input/Bindings/Runtime/RuntimeSingleButtonBinding.cs
@@ -10,10 +10,17 @@ namespace YARG.Input
 {
     public class RuntimeSingleButtonBinding : RuntimeSingleBinding<float>
     {
+        public bool Inverted { get; }
+        public DebounceMode DebounceMode { get; set; }
         public float PressPoint { get; }
-        public bool IsPressed => State >= PressPoint;
-
         private DebounceTimer<float> _debounceTimer;
+        public bool IsPressed => State >= PressPoint;
+        public bool WasPreviouslyPressed => PreviousState >= PressPoint;
+        public float PreviousState { get; private set; }
+
+        private float _invertSign => Inverted ? -1 : 1;
+
+
 
         public RuntimeSingleButtonBinding(InputControl<float> control, ReusableSingleButtonBinding reusableBinding) : base(control)
         {
@@ -21,6 +28,10 @@ namespace YARG.Input
             {
                 TimeThreshold = reusableBinding.DebounceThreshold,
             };
+
+            Inverted = reusableBinding.Inverted;
+            DebounceMode = reusableBinding.DebounceMode;
+            PressPoint = reusableBinding.PressPoint;
         }
 
         public void UpdateDebounce(double time)
@@ -31,6 +42,29 @@ namespace YARG.Input
             State = _debounceTimer.Stop();
             InvokeStateChanged(State);
             return;
+        }
+
+        public override void UpdateState(double time)
+        {
+            PreviousState = State;
+
+            // Read new state
+            _debounceTimer.UpdateValue(Control.value * _invertSign);
+
+            // Wait for debounce to end
+            if (!_debounceTimer.HasElapsed(time))
+                return;
+
+            State = _debounceTimer.Stop();
+
+            if (DebounceMode == DebounceMode.PressAndRelease ||
+                (IsPressed && DebounceMode == DebounceMode.Press) ||
+                (!IsPressed && DebounceMode == DebounceMode.Release))
+            {
+                _debounceTimer.Start(time);
+            }
+
+            InvokeStateChanged(State);
         }
     }
 }
